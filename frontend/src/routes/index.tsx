a76fb29d0d754d20a2ed3f402e4f193f447e1273
TaeSeung Yoo <remicon99@gmail.com>
2025-07-28 22:22:36 +0900

    fix: 화면 너비 수정

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className='text-center'>
-      <header className='flex min-h-screen w-2 flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white'>
+      <header className='flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white'>
         <p>토닥 서비스 준비중입니다.</p>
         <a
           className='text-[#61dafb] hover:underline'
