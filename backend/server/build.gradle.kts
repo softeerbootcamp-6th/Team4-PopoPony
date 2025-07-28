1d6de4a9254aca25b3132f583ace016277d0b577
seonghooni <lsh3717@naver.com>
2025-07-29 00:38:23 +0900

    Chore: Swagger 관련 dependency 추가

--- backend/server/build.gradle.kts
+++ backend/server/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
 	// ✅ JUnit5
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// ✅ Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 }
 
 val querydslDir = layout.buildDirectory.dir("generated/querydsl")
