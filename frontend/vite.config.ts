63d163d596f3266c711e71a65c08ebc0bfb9f31e
TaeSeung Yoo <remicon99@gmail.com>
2025-07-28 17:39:46 +0900

    feat: path alias 세팅 (tsconfigPaths)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,14 +11,6 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    tsconfigPaths(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
 })
