ff355af40d8d10bc1a369a42fc87ccaf1bfc5030
TaeSeung Yoo <remicon99@gmail.com>
2025-07-28 17:12:54 +0900

    feat: 임시 시작 페이지 퍼블리싱

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,24 +8,14 @@ function App() {
   return (
     <div className="text-center">
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
+        <p>토닥 서비스 준비중입니다.</p>
         <a
           className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
+          href="https://github.com/softeerbootcamp-6th/Team4-PopoPony"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
+          팀 레포지토리 바로가기
         </a>
       </header>
     </div>
