0bce1d036fcdb8ed4d69a65c2934c110405149b4
TaeSeung Yoo <remicon99@gmail.com>
2025-07-28 19:31:18 +0900

    chore: eslint 설정

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,51 @@
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
+/** @type {import("eslint").FlatConfig[]} */
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      react,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+      prettier,
+      '@tanstack/query': pluginQuery,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      ...prettierConfig.rules,
+
+      'prettier/prettier': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // 사용하지 않는 변수 경고
+      ...pluginQuery.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect', // React 버전 자동 감지
+      },
+    },
+  },
+];
