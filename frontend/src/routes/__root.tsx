7d43370f5fed0c48b744eb779c60f8971e01e131
TaeSeung Yoo <remicon99@gmail.com>
2025-07-28 19:24:56 +0900

    style: 코드 포맷팅

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,8 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import type { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import type { QueryClient } from '@tanstack/react-query'
 
 interface MyRouterContext {
   queryClient: QueryClient
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,23 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: App,
-})
+});
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+    <div className='text-center'>
+      <header className='flex min-h-screen w-2 flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white'>
         <p>토닥 서비스 준비중입니다.</p>
         <a
-          className="text-[#61dafb] hover:underline"
-          href="https://github.com/softeerbootcamp-6th/Team4-PopoPony"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='text-[#61dafb] hover:underline'
+          href='https://github.com/softeerbootcamp-6th/Team4-PopoPony'
+          target='_blank'
+          rel='noopener noreferrer'>
           팀 레포지토리 바로가기
         </a>
       </header>
     </div>
-  )
+  );
 }
