f9fc54187ce431b032b3dfe42f51a37bfe8dd291
TaeSeung Yoo <remicon99@gmail.com>
2025-07-30 00:58:44 +0900

    fix: LabeledSection 색상 오류 수정 (tailwind는 런타임 동적 클래스 x)

--- frontend/src/shared/components/LabeledSection.tsx
+++ frontend/src/shared/components/LabeledSection.tsx
@@ -11,7 +11,9 @@ const LabeledSection = ({ label, isChecked = false, children }: Props) => {
     <section className='flex flex-col gap-2'>
       <div className='flex items-center gap-2'>
         <h6 className='body1-16-bold text-neutral-90'>{label}</h6>
-        <IcCheck className={`[&_path]:stroke-${isChecked ? 'mint-50' : 'neutral-90'}`} />
+        <IcCheck
+          className={`${isChecked ? '[&_path]:stroke-mint-50' : '[&_path]:stroke-neutral-90'}`}
+        />
       </div>
       <div>{children}</div>
     </section>
