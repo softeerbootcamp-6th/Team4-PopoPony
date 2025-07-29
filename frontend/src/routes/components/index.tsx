ede013969f39d506a623d3889149b70776bcb7f2
TaeSeung Yoo <remicon99@gmail.com>
2025-07-30 00:05:41 +0900

    feat: LabeledSection, TwoOptionSelector 컴포넌트 예시 추가

--- frontend/src/routes/components/index.tsx
+++ frontend/src/routes/components/index.tsx
@@ -1,10 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Button } from '@components';
+import { Button, LabeledSection, TwoOptionSelector } from '@components';
+import { useForm, FormProvider, type SubmitHandler } from 'react-hook-form';
+
 export const Route = createFileRoute('/components/')({
   component: RouteComponent,
 });
 
+type FormValues = {
+  cognitive: string;
+};
+
 function RouteComponent() {
+  const methods = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log('Final Data:', data);
+  };
+
   return (
     <div className='p-[4rem]'>
       <h3 className='text-neutral-90 title-20-bold mb-[1.6rem]'>Button</h3>
@@ -45,6 +56,20 @@ function RouteComponent() {
         <Button variant='secondary' size='lg' isLoading>
           secondary lg
         </Button>
+      </div>
+      <div className='flex w-[33.5rem] flex-col gap-[1.6rem]'>
+        <FormProvider {...methods}>
+          <LabeledSection label='인지능력' isChecked={!!methods.watch('cognitive')}>
+            <TwoOptionSelector
+              name='cognitive'
+              leftOption={{ label: '괜찮아요', value: 'ok' }}
+              rightOption={{ label: '도움이 필요해요', value: 'help' }}
+            />
+          </LabeledSection>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <Button type='submit'>Submit</Button>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
