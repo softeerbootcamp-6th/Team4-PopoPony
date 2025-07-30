e924150d7396f777b0f4864a4984f24eb3e4e832
jaehyeonchoi <413cjh22@gmail.com>
2025-07-30 14:03:24 +0900

    Feat: BaseEntity에 auditing 및 soft delete 기능 구현
    - 생성시간, 수정시간, 삭제 시간 추적
    - @SQLDelete와 @Where를 통해 soft delete 처리 로직 구성

---
+++ backend/server/src/main/java/com/todoc/server/common/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.todoc.server.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE #{#entityName} SET deleted_at = NOW() WHERE id = ?")
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    protected LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+}
