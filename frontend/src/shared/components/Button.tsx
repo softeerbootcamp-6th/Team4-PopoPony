4c5b4cccd39e9d4e5556243ea21b0c3b34308810
TaeSeung Yoo <remicon99@gmail.com>
2025-07-30 17:23:30 +0900

    feat: button type 추가

--- frontend/src/shared/components/Button.tsx
+++ frontend/src/shared/components/Button.tsx
@@ -50,6 +50,7 @@ const Button = ({
 
   return (
     <button
+      type='button'
       className={`${baseStyle} ${disabled ? disabledVariantStyle[variant] : variantStyle[variant]} ${sizeStyle[size]}`}
       {...props}>
       {isLoading ? (
