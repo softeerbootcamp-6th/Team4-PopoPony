f86eb4276fa51c07897ae966cf33bff386211fce
TaeSeung Yoo <remicon99@gmail.com>
2025-07-31 01:03:06 +0900

    feat: 데스크탑뷰 랜딩 뷰 퍼블리싱

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Outlet, createRootRouteWithContext, redirect } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { QueryClient } from '@tanstack/react-query';
+import { Landing } from '@components';
+import RootLayout from './_layout';
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -23,7 +25,12 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 
   component: () => (
     <>
-      <Outlet />
+      <RootLayout>
+        <Landing />
+        <div className='h-[100dvh] min-h-[100dvh] w-full max-w-[500px] min-w-[375px]'>
+          <Outlet />
+        </div>
+      </RootLayout>
       <div className='text-[16px]'>
         <TanStackRouterDevtools />
         <ReactQueryDevtools buttonPosition='bottom-right' />
