0a29f5551461e79cf8ea2849ba757d7a1ddc18b1
jaehyeonchoi <413cjh22@gmail.com>
2025-07-31 03:12:06 +0900

    Feat: 리뷰 정보 관리를 위한 Review 엔티티 생성
    - BaseEntity 상속으로 생성/수정/삭제일 관리

---
+++ backend/server/src/main/java/com/todoc/server/domain/review/entity/Review.java
@@ -0,0 +1,40 @@
+package com.todoc.server.domain.review.entity;
+
+import com.todoc.server.common.enumeration.SatisfactionLevel;
+import com.todoc.server.common.entity.BaseEntity;
+import com.todoc.server.domain.escort.entity.Escort;
+import com.todoc.server.domain.helper.entity.Helper;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "escort_id")
+    private Escort escort;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "helper_id")
+    private Helper helper;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "satisfaction_level")
+    private SatisfactionLevel satisfactionLevel;
+
+    @Column(columnDefinition = "json",
+            name = "positive_feedback")
+    private String positiveFeedback;
+
+    @Column(name = "negative_feedback")
+    private String negativeFeedback;
+
+    @Column(name = "short_comment")
+    private String shortComment;
+}
