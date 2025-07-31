5cc09ed56c290635289ea41b4eee9da5513c8a65
TaeSeung Yoo <remicon99@gmail.com>
2025-07-31 18:53:58 +0900

    feat: 랜딩 페이지 퍼블리싱

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -6,10 +6,17 @@ export const Route = createFileRoute('/')({
 
 const landingButtonData = [
   {
-    title: '토닥과 함께, 투-닥터',
-    description: '믿을 수 있는 병원 동행 파트너 토닥과 함께, 도우미 매칭부터 실시간 동행 확인까지 안심하고 이용해보세요.',
+    subtitle: '보호자 서비스',
+    title: '병원 동행 신청하기',
+    image: '/images/hospital.png',
+    link: '/customer',
   },
-  
+  {
+    subtitle: '도우미 서비스',
+    title: '일감 찾기',
+    image: '/images/work.png',
+    link: '/helper',
+  },
 ];
 
 function App() {
@@ -20,9 +27,25 @@ function App() {
         <h2 className='title-20-bold text-text-neutral-secondary'>토닥과 함께, 투-닥터</h2>
       </div>
 
-      <div className='flex-col-center mt-[4rem] gap-[1.6rem]'>
-        <div className='from-neutral-5 to-neutral-10 h-[12rem] w-full rounded-[2rem] bg-gradient-to-b'></div>
-        <div className='from-neutral-5 to-neutral-10 h-[12rem] w-full rounded-[2rem] bg-gradient-to-b'></div>
+      <div className='flex-col-center mt-[4rem] cursor-pointer gap-[1.6rem]'>
+        {landingButtonData.map((data) => (
+          <Link
+            key={data.title}
+            to={data.link}
+            className='from-neutral-5 to-neutral-10 relative flex h-[12rem] w-full justify-between rounded-[2rem] bg-gradient-to-b p-[1.6rem]'>
+            <div className='z-10'>
+              <h3 className='body2-14-bold text-text-mint-primary'>{data.subtitle}</h3>
+              <h2 className='headline-24-bold text-text-neutral-primary mt-[0.4rem]'>
+                {data.title}
+              </h2>
+            </div>
+            <img
+              src={data.image}
+              alt={data.title}
+              className='absolute right-[1.6rem] max-h-[8.8rem]'
+            />
+          </Link>
+        ))}
       </div>
     </div>
   );
