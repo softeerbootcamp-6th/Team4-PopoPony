45fe7f651a533ec8b4ac4fd235ae8fe8f57d7fef
TaeSeung Yoo <remicon99@gmail.com>
2025-08-01 10:41:01 +0900

    fix: 랜딩 페이지 오타 수정

--- frontend/src/shared/components/Landing.tsx
+++ frontend/src/shared/components/Landing.tsx
@@ -4,7 +4,7 @@ const Landing = () => {
       <img src='/images/logo-text.svg' alt='logo-text' className='ml-[6.6rem] w-[11rem]' />
       <img src='/images/landing.png' alt='landing' className='mt-[3rem] w-[46rem]' />
       <div className='absolute bottom-[-11rem] left-[6.6rem]'>
-        <h2 className='display-32-bold text-neutral-100'>토닥과 함께, 투-닥터</h2>
+        <h2 className='display-32-bold text-neutral-100'>토닥과 함께, 투-닥터!</h2>
         <p className='body1-18-medium text-text-neutral-secondary mt-[1.6rem]'>
           {`믿을 수 있는 병원 동행 파트너 토닥과 함께,\n도우미 매칭부터 실시간 동행 확인까지 안심하고 이용해보세요.`}
         </p>
