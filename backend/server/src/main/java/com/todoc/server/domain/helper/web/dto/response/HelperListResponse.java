c58d32836b7f94efeb5361dab4a8db0338780e60
jaehyeonchoi <413cjh22@gmail.com>
2025-08-03 01:07:19 +0900

    Feat: 도우미 목록 응답에 대한 DTO 클래스 HelperListResponse 추가

---
+++ backend/server/src/main/java/com/todoc/server/domain/helper/web/dto/response/HelperListResponse.java
@@ -0,0 +1,20 @@
+package com.todoc.server.domain.helper.web.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "도우미 목록 조회 응답 DTO")
+public class HelperListResponse {
+
+    @Schema(description = "도우미 목록")
+    private List<HelperSimpleResponse> helperList;
+
+    @Builder
+    public HelperListResponse(List<HelperSimpleResponse> helperList) {
+        this.helperList = helperList;
+    }
+}
