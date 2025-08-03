86d51a0eec2ebf3b3292bd69086260fdb4047bbd
seonghooni <lsh3717@naver.com>
2025-08-04 00:55:15 +0900

    Feat: 도우미가 조회하는 홈 화면에서의 Recruit 목록 조회 Controller 추가

--- backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/RecruitController.java
+++ backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/RecruitController.java
@@ -72,6 +72,43 @@ public class RecruitController {
     }
 
     @Operation(
+        summary = "도우미의 동행 목록 조회",
+        description = "로그인한 도우미가 지원한 동행 목록(진행중/완료)을 조회합니다.")
+    @ApiResponse(
+        responseCode = "200",
+        description = "동행 목록 조회 성공",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = Response.class)
+        ))
+    @GetMapping("/helper")
+    public Response<RecruitListResponse> getRecruitListAsHelper() {
+        // TODO :: 원래라면 jwt 혹은 sessionId로부터 유저 정보를 조회해야 함
+        // 현재는 우선 userId = 1로 고정
+
+        RecruitSimpleResponse dto = RecruitSimpleResponse.builder()
+            .recruitId(1L)
+            .status(RecruitStatus.MATCHING)
+            .numberOfApplication(3L)
+            .destination("서울아산병원")
+            .departureLocation("꿈에그린아파트")
+            .escortDate(LocalDate.now())
+            .estimatedMeetingTime(LocalTime.NOON)
+            .estimatedReturnTime(LocalTime.MIDNIGHT)
+            .build();
+
+        List<RecruitSimpleResponse> list = new ArrayList<>();
+        list.add(dto);
+        list.add(dto);
+
+        RecruitListResponse mock = RecruitListResponse.builder()
+            .completedList(list)
+            .inProgressList(list).build();
+
+        return Response.from(mock);
+    }
+
+    @Operation(
             summary = "특정 동행 신청의 상세 정보 조회",
             description = "recruitId에 해당하는 동행 신청의 상세 정보를 조회합니다.")
     @ApiResponse(
