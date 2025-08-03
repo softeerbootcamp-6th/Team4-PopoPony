9f9eec4fc793313bdc361f9776f5263f13267a3a
jaehyeonchoi <413cjh22@gmail.com>
2025-08-03 22:20:55 +0900

    Feat: HelperNotFoundException - 커스텀 예외 추가

---
+++ backend/server/src/main/java/com/todoc/server/domain/helper/exception/HelperNotFoundException.java
@@ -0,0 +1,9 @@
+package com.todoc.server.domain.helper.exception;
+
+import com.todoc.server.common.exception.base.CustomException;
+
+public class HelperNotFoundException extends CustomException {
+    public HelperNotFoundException() {
+        super(HelperResponseCode.HELPER_NOT_FOUND);
+    }
+}
