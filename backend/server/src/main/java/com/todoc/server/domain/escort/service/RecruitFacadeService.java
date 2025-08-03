ce846df4bafcb266416be4264ef1cf14bd630ede
seonghooni <lsh3717@naver.com>
2025-08-03 20:28:33 +0900

    Feat: 동행 신청 과정에서 발생하는 비즈니스 로직을 호출하는 RecruitFacadeService 생성

---
+++ backend/server/src/main/java/com/todoc/server/domain/escort/service/RecruitFacadeService.java
@@ -0,0 +1,55 @@
+package com.todoc.server.domain.escort.service;
+
+import com.todoc.server.domain.auth.entity.Auth;
+import com.todoc.server.domain.customer.entity.Patient;
+import com.todoc.server.domain.customer.service.PatientService;
+import com.todoc.server.domain.escort.entity.Recruit;
+import com.todoc.server.domain.escort.web.dto.request.RecruitCreateRequest;
+import com.todoc.server.domain.route.entity.LocationInfo;
+import com.todoc.server.domain.route.entity.Route;
+import com.todoc.server.domain.route.service.LocationInfoService;
+import com.todoc.server.domain.route.service.RouteService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RecruitFacadeService {
+
+    private final RecruitService recruitService;
+    private final PatientService patientService;
+    private final LocationInfoService locationInfoService;
+    private final RouteService routeService;
+
+    public void createRecruit(RecruitCreateRequest request) {
+        // TODO :: 세션 혹은 JWT로부터 고객 정보 가져오기
+        Auth customer = Auth.builder()
+                .id(1L)
+                .build();
+
+        // 환자 정보 저장 (생성 + 연관관계 설정)
+        Patient patient = patientService.register(request);
+        patient.setCustomer(customer);
+
+        // 위치 정보 저장 (생성 + 연관관계 설정)
+        LocationInfo meetingLocation = locationInfoService.register(request);
+        LocationInfo hospitalLocation = locationInfoService.register(request);
+        LocationInfo returnLocation = locationInfoService.register(request);
+
+        // 경로 정보 저장 (생성 + 연관관계 설정)
+        Route route = routeService.register(request);
+        route.setMeetingLocationInfo(meetingLocation);
+        route.setHospitalLocationInfo(hospitalLocation);
+        route.setReturnLocationInfo(returnLocation);
+
+        // 동행 신청 생성 (생성 + 연관관계 설정)
+        Recruit recruit = recruitService.register(request);
+        recruit.setCustomer(customer);
+        recruit.setPatient(patient);
+        recruit.setRoute(route);
+        // TODO :: 경로 API 로부터 금액 가져와야 함
+        recruit.setEstimatedFee(null);
+    }
+}
