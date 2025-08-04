dc7c46ec85b1339272ba3202420d2234022c927e
TaeSeung Yoo <remicon99@gmail.com>
2025-08-04 18:10:49 +0900

    Feat: 동행 상세 페이지 레이아웃 퍼블리싱

--- frontend/src/routes/customer/escort/$escortId/index.tsx
+++ frontend/src/routes/customer/escort/$escortId/index.tsx
@@ -1,3 +1,5 @@
+import { EscortCard, Tabs } from '@components';
+import { PageLayout } from '@layouts';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/customer/escort/$escortId/')({
@@ -5,5 +7,35 @@ export const Route = createFileRoute('/customer/escort/$escortId/')({
 });
 
 function RouteComponent() {
-  return <div>Hello "/customer/escort/$escortId/"!</div>;
+  return (
+    <PageLayout>
+      <PageLayout.Header title='내역 상세보기' showBack />
+      <PageLayout.Content>
+        <div className='bg-neutral-10 px-[2rem] py-[1.6rem]'>
+          <EscortCard>
+            <EscortCard.StatusHeader
+              text='동행번호 NO.12394O4L'
+              title='7월 22일 (토), 서울아산병원'
+            />
+            <EscortCard.Divider />
+            <EscortCard.InfoSection>
+              <EscortCard.Info type='time' text='7월 22일(토) 12시 ~ 15시' />
+              <EscortCard.Info type='location' text='꿈에그린아파트 → 서울아산병원' />
+            </EscortCard.InfoSection>
+          </EscortCard>
+        </div>
+
+        <Tabs defaultValue='도우미'>
+          <Tabs.TabsList>
+            <Tabs.TabsTrigger value='도우미'>도우미</Tabs.TabsTrigger>
+            <Tabs.TabsTrigger value='신청 내역'>신청 내역</Tabs.TabsTrigger>
+          </Tabs.TabsList>
+          <Tabs.TabsContent value='도우미'>
+            <div className='flex-col-start gap-[1.2rem] p-[2rem]'></div>
+          </Tabs.TabsContent>
+          <Tabs.TabsContent value='신청 내역'>Change your 신청 내역 here.</Tabs.TabsContent>
+        </Tabs>
+      </PageLayout.Content>
+    </PageLayout>
+  );
 }
