19995de16f61d70fcefd9a96427e999945cec7bc
seonghooni <lsh3717@naver.com>
2025-08-04 20:59:02 +0900

    Refactor: Area enum 클래스에서 시/도에 대한 값 변경 - 외부 API의 시/도 분류와 같도록 설정

--- backend/server/src/main/java/com/todoc/server/common/enumeration/Area.java
+++ backend/server/src/main/java/com/todoc/server/common/enumeration/Area.java
@@ -1,23 +1,23 @@
 package com.todoc.server.common.enumeration;
 
 public enum Area {
-    SEOUL("서울특별시"),
-    BUSAN("부산광역시"),
-    DAEGU("대구광역시"),
-    INCHEON("인천광역시"),
-    GWANGJU("광주광역시"),
-    DAEJEON("대전광역시"),
-    ULSAN("울산광역시"),
-    SEJONG("세종특별자치시"),
-    GYEONGGI("경기도"),
-    GANGWON("강원특별자치도"),
-    CHUNGBUK("충청북도"),
-    CHUNGNAM("충청남도"),
-    JEONBUK("전라북도"),
-    JEONNAM("전라남도"),
-    GYEONGBUK("경상북도"),
-    GYEONGNAM("경상남도"),
-    JEJU("제주특별자치도");
+    SEOUL("서울"),
+    BUSAN("부산"),
+    DAEGU("대구"),
+    INCHEON("인천"),
+    GWANGJU("광주"),
+    DAEJEON("대전"),
+    ULSAN("울산"),
+    SEJONG("세종시"),
+    GYEONGGI("경기"),
+    GANGWON("강원"),
+    CHUNGBUK("충북"),
+    CHUNGNAM("충남"),
+    JEONBUK("전북"),
+    JEONNAM("전남"),
+    GYEONGBUK("경북"),
+    GYEONGNAM("경남"),
+    JEJU("제주");
 
     private final String label;
 
