ec75b637a21d6292e4d67f23823cc4313e04ba2b
jaehyeonchoi <413cjh22@gmail.com>
2025-08-04 14:48:36 +0900

    Fix: ReviewSimpleResponse - 생성자 접근지정자 public으로 변경

--- backend/server/src/main/java/com/todoc/server/domain/review/web/dto/response/ReviewSimpleResponse.java
+++ backend/server/src/main/java/com/todoc/server/domain/review/web/dto/response/ReviewSimpleResponse.java
@@ -1,30 +1,35 @@
 package com.todoc.server.domain.review.web.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.todoc.server.common.enumeration.SatisfactionLevel;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Schema(description = "도우미 후기 요약 정보 DTO")
 public class ReviewSimpleResponse {
 
+    @Schema(description = "리뷰 ID")
+    private Long reviewId;
+
     @Schema(description = "만족도", allowableValues = {"GOOD", "AVERAGE", "BAD"})
     private SatisfactionLevel satisfactionLevel;
 
     @Schema(description = "작성일")
-    private LocalDate createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 
-    @Column(name = "한 줄 코멘트")
+    @Schema(description = "한 줄 코멘트")
     private String shortComment;
 
     @Builder
-    public ReviewSimpleResponse(SatisfactionLevel satisfactionLevel, LocalDate createdAt, String shortComment) {
+    public ReviewSimpleResponse(Long reviewId, SatisfactionLevel satisfactionLevel, String shortComment, LocalDateTime createdAt) {
+        this.reviewId = reviewId;
         this.satisfactionLevel = satisfactionLevel;
-        this.createdAt = createdAt;
         this.shortComment = shortComment;
+        this.createdAt = createdAt;
     }
 }
