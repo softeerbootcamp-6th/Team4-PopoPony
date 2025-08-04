533283130cf4c12108939e90b7725b7e0858420e
jaehyeonchoi <413cjh22@gmail.com>
2025-08-04 16:04:44 +0900

    Refactor: AuthNotFoundException - Auth 정보를 찾지 못했을 때의 예외를 정의
    - AuthException은 제거

--- backend/server/src/main/java/com/todoc/server/domain/auth/exception/AuthException.java
+++
@@ -1,9 +0,0 @@
-package com.todoc.server.domain.auth.exception;
-
-import com.todoc.server.common.exception.base.CustomException;
-
-public class AuthException extends CustomException {
-    public AuthException(AuthErrorCode authErrorCode) {
-        super(authErrorCode);
-    }
-}
