af0ad1d1ca00f2f50ffed9257abb8c27661846ec
yjbae3974 <brianbae6@gmail.com>
2025-08-04 14:10:00 +0900

    Feat: formlayout 수정 및 변경사항 반영

--- frontend/src/features/customer/components/recruitStep/step1.tsx
+++ frontend/src/features/customer/components/recruitStep/step1.tsx
@@ -1,40 +1,63 @@
 // frontend/src/features/customer/components/recruitStep/step1.tsx
-import { TwoOptionSelector, FormInput, LabeledSection, PhotoUpload } from '@components';
+import { TwoOptionSelector, FormInput, LabeledSection, PhotoUpload, Button } from '@components';
 import { useWatch } from 'react-hook-form';
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
+import { FormLayout } from '@layouts';
 
 type Props = {
   onValidChange: (isValid: boolean) => void;
 };
-
-function isFilled(v: unknown) {
-  return !(v === undefined || v === null || v === '' || (Array.isArray(v) && v.length === 0));
-}
 
 const Step1 = memo(({ onValidChange }: Props) => {
   const nameValue = useWatch({ name: 'patientName' });
   const leftValue = useWatch({ name: 'step1' });
   const photoValue = useWatch({ name: 'photo' });
 
-  // 필수 값 모두 채워졌는지 판단해서 상위로 전달
-  useEffect(() => {
-    const valid = isFilled(nameValue) && isFilled(leftValue) && isFilled(photoValue);
-    onValidChange(valid);
-  }, [nameValue, leftValue, photoValue, onValidChange]);
+  // // 필수 값 모두 채워졌는지 판단해서 상위로 전달
 
   return (
     <>
-      <PhotoUpload name='photo' />
-      <LabeledSection label='왼쪽' isChecked={!!leftValue}>
-        <TwoOptionSelector
-          name='step1'
-          leftOption={{ label: '왼쪽', value: 'step1-left' }}
-          rightOption={{ label: '오른쪽', value: 'step1-right' }}
-        />
-      </LabeledSection>
-      <LabeledSection label='환자 이름' isChecked={!!nameValue}>
-        <FormInput type='text' size='M' placeholder='이름 입력' name='patientName' />
-      </LabeledSection>
+      <FormLayout>
+        <FormLayout.Content>
+          <FormLayout.Title>
+            <h2>동행할 환자의 기본정보를 입력해주세요</h2>
+          </FormLayout.Title>
+          <FormLayout.SubTitle>
+            <Button variant='assistive'>이전 환자 정보 불러오기</Button>
+          </FormLayout.SubTitle>
+          <div className='flex-center w-full'>
+            <PhotoUpload name='photo' />
+          </div>
+          <LabeledSection label='왼쪽' isChecked={!!leftValue}>
+            <TwoOptionSelector
+              name='step1'
+              leftOption={{ label: '왼쪽', value: 'step1-left' }}
+              rightOption={{ label: '오른쪽', value: 'step1-right' }}
+            />
+          </LabeledSection>
+          <LabeledSection label='환자 이름' isChecked={!!nameValue}>
+            <FormInput type='text' size='M' placeholder='이름 입력' name='patientName' />
+          </LabeledSection>
+          <div className='flex-center w-full'>
+            <PhotoUpload name='photo' />
+          </div>
+          <LabeledSection label='왼쪽' isChecked={!!leftValue}>
+            <TwoOptionSelector
+              name='step1'
+              leftOption={{ label: '왼쪽', value: 'step1-left' }}
+              rightOption={{ label: '오른쪽', value: 'step1-right' }}
+            />
+          </LabeledSection>
+          <LabeledSection label='환자 이름' isChecked={!!nameValue}>
+            <FormInput type='text' size='M' placeholder='이름 입력' name='patientName' />
+          </LabeledSection>
+        </FormLayout.Content>
+        <FormLayout.Footer>
+          <Button variant='primary' disabled={!(leftValue && nameValue && photoValue)}>
+            다음
+          </Button>
+        </FormLayout.Footer>
+      </FormLayout>
     </>
   );
 });
---
+++ frontend/src/shared/layouts/FormLayout.tsx
@@ -0,0 +1,45 @@
+import { BottomCTA } from '@components';
+
+interface FormLayoutProps {
+  children: React.ReactNode;
+}
+
+const FormLayout = ({ children }: FormLayoutProps) => {
+  return <div className='bg-background-default-white flex h-full w-full flex-col'>{children}</div>;
+};
+
+const Title = ({ className, children }: { className?: string; children: React.ReactNode }) => {
+  return (
+    <div className={`headline-24-bold text-text-neutral-primary ${className}`}>
+      <h2>{children}</h2>
+    </div>
+  );
+};
+const SubTitle = ({ className, children }: { className?: string; children: React.ReactNode }) => {
+  return (
+    <div className={`body1-16-medium text-text-neutral-secondary pt-[0.8rem] ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+const Content = ({ children }: FormLayoutProps) => {
+  return (
+    <div className='flex flex-1 flex-col gap-[3.2rem] overflow-y-auto p-[2rem]'>{children}</div>
+  );
+};
+
+const Footer = ({ children }: FormLayoutProps) => {
+  return (
+    <div className='flex-shrink-0'>
+      <BottomCTA>{children}</BottomCTA>
+    </div>
+  );
+};
+
+FormLayout.Title = Title;
+FormLayout.SubTitle = SubTitle;
+FormLayout.Content = Content;
+FormLayout.Footer = Footer;
+
+export default FormLayout;
