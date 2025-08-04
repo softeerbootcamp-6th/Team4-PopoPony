72dd68816b90dba33051e208a175a626fa89eb0b
jaehyeonchoi <413cjh22@gmail.com>
2025-08-05 02:43:51 +0900

    Feat: RecruitNotFoundException - 동행 신청이 존재하지 않는 경우에 대한 커스텀 예외를 정의

---
+++ backend/server/src/main/java/com/todoc/server/domain/escort/exception/RecruitNotFoundException.java
@@ -0,0 +1,9 @@
+package com.todoc.server.domain.escort.exception;
+
+import com.todoc.server.common.exception.base.CustomException;
+
+public class RecruitNotFoundException extends CustomException {
+    public RecruitNotFoundException() {
+        super(EscortErrorCode.NOT_FOUND);
+    }
+}
