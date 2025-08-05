304993bb47a142ba7a87893c3a78c06d032cc4da
jaehyeonchoi <413cjh22@gmail.com>
2025-08-05 12:13:18 +0900

    Feat: RecruitStatus - 동행 신청 취소 상태 추가

--- backend/server/src/main/java/com/todoc/server/common/enumeration/RecruitStatus.java
+++ backend/server/src/main/java/com/todoc/server/common/enumeration/RecruitStatus.java
@@ -7,7 +7,8 @@ public enum RecruitStatus {
     MATCHING("매칭중"),
     COMPLETED("매칭완료"),
     IN_PROGRESS("동행중"),
-    DONE("완료된 동행");
+    DONE("완료된 동행"),
+    CANCELLED("취소됨");
 
     private final String label;
 
