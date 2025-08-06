52f3d08e544349f9d312ff726cc24cfead484eda
jaehyeonchoi <413cjh22@gmail.com>
2025-08-06 20:29:21 +0900

    Feat: ApplicationNotFoundException - 지원을 찾을 수 없는 경우에 대한 커스텀 예외 정의

--- backend/server/src/main/java/com/todoc/server/domain/escort/exception/EscortErrorCode.java
+++ backend/server/src/main/java/com/todoc/server/domain/escort/exception/EscortErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum EscortErrorCode implements ResponseCode {
     RECRUIT_NOT_FOUND(130101, HttpStatus.NOT_FOUND.value(), "해당 동행 신청을 찾을 수 없습니다."),
-    RECRUIT_INVALID_CANCEL(130103, HttpStatus.BAD_REQUEST.value(), "매칭이 완료된 동행 신청은 취소할 수 없습니다.");
+    RECRUIT_INVALID_CANCEL(130103, HttpStatus.BAD_REQUEST.value(), "매칭이 완료된 동행 신청은 취소할 수 없습니다."),
+
+    APPLICATION_NOT_FOUND(130201, HttpStatus.NOT_FOUND.value(), "해당 동행 지원을 찾을 수 없습니다.");
 
     private final int code;
     private final int status;
