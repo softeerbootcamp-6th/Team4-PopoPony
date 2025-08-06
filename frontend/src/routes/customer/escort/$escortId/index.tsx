56376b50bd26f565cefb86af4e7ea772556b3e89
TaeSeung Yoo <remicon99@gmail.com>
2025-08-06 15:59:40 +0900

    Feat: 내역 상세보기 도우미 탭 퍼블리싱

--- frontend/src/routes/customer/escort/$escortId/index.tsx
+++ frontend/src/routes/customer/escort/$escortId/index.tsx
@@ -17,7 +17,7 @@ import {
   RouteButton,
 } from '@customer/components';
 import { useModal } from '@hooks';
-import { IcCheck } from '@icons';
+import { IcAlertCircle, IcCheck } from '@icons';
 import { PageLayout } from '@layouts';
 import { createFileRoute } from '@tanstack/react-router';
 
@@ -100,6 +100,85 @@ function RouteComponent() {
               <ReportInfoCard />
               <PaymentFailedCard />
             </Tabs.TabsCotentSection>
+            <Tabs.TabsDivider />
+            <Tabs.TabsCotentSection gap='3.6rem'>
+              <div>
+                <h3 className='subtitle-18-bold text-text-neutral-primary'>동행 리포트</h3>
+                <div className='mt-[1.2rem] flex flex-col gap-[2rem]'>
+                  <InfoSection title='진행 시간'>
+                    <div>
+                      <span className='body1-16-medium text-text-neutral-primary'>
+                        오후 12시 ~ 오후 3시 30분
+                      </span>
+                      <div className='flex-start gap-[0.4rem]'>
+                        <IcAlertCircle
+                          className='[&_path]:fill-icon-red-primary'
+                          width={16}
+                          height={16}
+                        />
+                        <span className='label3-12-medium text-red-50'>
+                          예상 동행 시간보다 30분 초과되었어요!
+                        </span>
+                      </div>
+                    </div>
+                  </InfoSection>
+                  <Divider />
+                  <InfoSection title='다음 예약'>
+                    <div>
+                      <span className='body1-16-medium text-text-neutral-primary'>
+                        2025년 7월 31일 오후 1시
+                      </span>
+                    </div>
+                  </InfoSection>
+                  <Divider />
+                  <InfoSection title='전달 내용'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col rounded-[0.6rem] p-[1rem]'>
+                      <span>오늘 치료 잘 받으셨고, 약은 가방 안에 넣어드렸습니다.</span>
+                    </div>
+                  </InfoSection>
+                </div>
+              </div>
+              <div>
+                <h3 className='subtitle-18-bold text-text-neutral-primary'>총 이용 금액</h3>
+                <div className='mt-[1.2rem] flex flex-col gap-[2rem]'>
+                  <div className='flex-between'>
+                    <span className='body2-14-medium text-text-neutral-secondary'>
+                      기존 결제금액
+                    </span>
+                    <span className='body1-16-bold text-text-neutral-primary'>100,000원</span>
+                  </div>
+                  <Divider />
+                  <div className='flex flex-col gap-[0.8rem]'>
+                    <div className='flex-between'>
+                      <span className='body2-14-medium text-text-neutral-secondary'>
+                        추가 결제금액
+                      </span>
+                      <span className='body1-16-bold text-text-neutral-primary'>70,000원</span>
+                    </div>
+                    <div className='flex-between'>
+                      <span className='body2-14-medium text-text-neutral-assistive'>택시 요금</span>
+                      <span className='label3-12-medium text-text-neutral-secondary'>50,000원</span>
+                    </div>
+                    <div className='flex-between'>
+                      <span className='body2-14-medium text-text-neutral-assistive'>
+                        이용 시간 초과 요금
+                      </span>
+                      <span className='label3-12-medium text-text-neutral-secondary'>20,000원</span>
+                    </div>
+                    <div className='flex-start bg-background-light-red gap-[0.8rem] rounded-[0.6rem] p-[1rem]'>
+                      <IcAlertCircle
+                        className='[&_path]:fill-icon-red-primary'
+                        width={16}
+                        height={16}
+                      />
+                      <span className='caption1-12-medium text-text-red-primary'>
+                        이용시간 초과요금은 15분에 5천원이에요.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Tabs.TabsCotentSection>
           </Tabs.TabsContent>
           <Tabs.TabsContent value='신청 내역'>
             <Tabs.TabsCotentSection>
@@ -138,7 +217,7 @@ function RouteComponent() {
               <RouteButton />
             </Tabs.TabsCotentSection>
             <Tabs.TabsDivider />
-            <Tabs.TabsCotentSection gap='gap-[2.4rem]'>
+            <Tabs.TabsCotentSection gap='2.4rem'>
               <div className='flex flex-col gap-[1.6rem]'>
                 <h3 className='subtitle-18-bold text-text-neutral-primary'>환자 상태</h3>
                 <div className='flex flex-col gap-[2rem]'>
@@ -150,7 +229,7 @@ function RouteComponent() {
                     </div>
                   </InfoSection>
                   <InfoSection title='인지 능력' status='괜찮아요'>
-                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col rounded-[0.6rem] p-[0.8rem]'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col rounded-[0.6rem] p-[1rem]'>
                       <div className='flex-start'>
                         <IcCheck />
                         <span>판단에 도움이 필요해요</span>
@@ -162,7 +241,7 @@ function RouteComponent() {
                     </div>
                   </InfoSection>
                   <InfoSection title='의사소통' status='도움이 필요해요'>
-                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex gap-[0.8rem] rounded-[0.6rem] p-[0.8rem]'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary rounded-[0.6rem] p-[1rem]'>
                       <span>이가 많이 없으셔서 발음하시는 게 불편하세요.</span>
                     </div>
                   </InfoSection>
@@ -173,17 +252,17 @@ function RouteComponent() {
                 <h3 className='subtitle-18-bold text-text-neutral-primary'>진료시 참고 사항</h3>
                 <div className='flex flex-col gap-[2rem]'>
                   <InfoSection title='특이사항'>
-                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col gap-[0.8rem] rounded-[0.6rem] p-[0.8rem]'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary rounded-[0.6rem] p-[1rem]'>
                       <span>이가 많이 없으셔서 발음하시는 게 불편하세요.</span>
                     </div>
                   </InfoSection>
                   <InfoSection title='동행 목적'>
-                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col gap-[0.8rem] rounded-[0.6rem] p-[0.8rem]'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary rounded-[0.6rem] p-[1rem]'>
                       <span>이가 많이 없으셔서 발음하시는 게 불편하세요.</span>
                     </div>
                   </InfoSection>
                   <InfoSection title='요청사항'>
-                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col gap-[0.8rem] rounded-[0.6rem] p-[0.8rem]'>
+                    <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary rounded-[0.6rem] p-[1rem]'>
                       <span>다음 정기 검진 예약 꼭 잡아주세요!</span>
                     </div>
                   </InfoSection>
@@ -210,6 +289,11 @@ function RouteComponent() {
           </Tabs.TabsContent>
         </Tabs>
       </PageLayout.Content>
+      <PageLayout.Footer>
+        <div className='flex-start gap-[1.2rem]'>
+          <Button onClick={() => {}}>도우미 후기 남기기</Button>
+        </div>
+      </PageLayout.Footer>
     </PageLayout>
   );
 }
