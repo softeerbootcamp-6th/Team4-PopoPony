30a751770c896bc304b91d5b0c4f7d25cee46ebe
jaehyeonchoi <413cjh22@gmail.com>
2025-08-06 12:10:10 +0900

    Refactor: HelperService - 사용하지 않는 메서드 제거

--- backend/server/src/main/java/com/todoc/server/domain/helper/service/HelperService.java
+++ backend/server/src/main/java/com/todoc/server/domain/helper/service/HelperService.java
@@ -1,8 +1,7 @@
 package com.todoc.server.domain.helper.service;
 
-import com.todoc.server.domain.helper.entity.Helper;
-import com.todoc.server.domain.helper.exception.HelperNotFoundException;
-import com.todoc.server.domain.helper.repository.HelperJpaRepository;
+import com.todoc.server.domain.helper.repository.HelperQueryRepository;
+import com.todoc.server.domain.helper.web.dto.response.HelperSimpleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,17 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class HelperService {
 
-    private final HelperJpaRepository helperJpaRepository;
+    private final HelperQueryRepository helperQueryRepository;
 
     /**
-     * userId(authId)에 해당하는 도우미를 조회하는 함수
+     * helperProfileId에 해당하는 도우미 요약 정보를 조회하는 함수
      *
-     * @param userId (authId)
-     * @return Helper 인스턴스
+     * @param helperProfileId 도우미 프로필 ID
+     * @return HelperSimpleResponse 인스턴스
      */
     @Transactional(readOnly = true)
-    public Helper getHelperByUserId(Long userId) {
-        return helperJpaRepository.findByAuthId(userId)
-                .orElseThrow(HelperNotFoundException::new);
+    public HelperSimpleResponse getHelperSimpleByHelperProfileId(Long helperProfileId) {
+        return helperQueryRepository.getHelperSimpleByHelperProfileId(helperProfileId);
     }
 }
