b6dd7a41e7bc5ac6fd04d73f5d3f18b22f6210ae
TaeSeung Yoo <remicon99@gmail.com>
2025-08-06 14:02:54 +0900

    Fix: 지도보기 버튼 스타일 수정

--- frontend/src/shared/components/ShowMapButton.tsx
+++ frontend/src/shared/components/ShowMapButton.tsx
@@ -5,11 +5,9 @@ interface ShowMapButtonProps {
 
 const ShowMapButton = ({ roadAddress, businessAddress }: ShowMapButtonProps) => {
   return (
-    <div className='body2-14-medium text-text-neutral-secondary flex-start gap-[0.4rem]'>
-      <span className='mr-[0.4rem]'>{roadAddress}</span>
-      <span className='mr-[0.4rem]'>{businessAddress}</span>
-      <div></div>
-      <button className='caption2-10-medium text-text-neutral-secondary border-stroke-neutral-dark w-fit rounded-[0.4rem] border px-[0.5rem] py-[0.2rem]'>
+    <div className='body2-14-medium text-text-neutral-secondary flex-wrap gap-[0.4rem]'>
+      <span className='mr-[0.4rem]'>{`${roadAddress}, ${businessAddress}`}</span>
+      <button className='caption2-10-medium text-text-neutral-secondary border-stroke-neutral-dark w-fit translate-y-[-0.1rem] rounded-[0.4rem] border px-[0.5rem] py-[0.2rem]'>
         지도 보기
       </button>
     </div>
