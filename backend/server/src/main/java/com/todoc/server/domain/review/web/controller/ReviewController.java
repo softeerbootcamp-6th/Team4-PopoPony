38e4ff8d298a5dac61812e57784e1d09d250363b
jaehyeonchoi <413cjh22@gmail.com>
2025-08-06 12:30:57 +0900

    Refactor: ReportController - 파사드 계층을 거치지 않고, 바로 서비스를 호출하도록 수정

--- backend/server/src/main/java/com/todoc/server/domain/review/web/controller/ReviewController.java
+++ backend/server/src/main/java/com/todoc/server/domain/review/web/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.todoc.server.domain.review.web.controller;
 
 import com.todoc.server.common.enumeration.SatisfactionLevel;
 import com.todoc.server.common.response.Response;
+import com.todoc.server.domain.review.service.ReviewService;
 import com.todoc.server.domain.review.web.dto.response.ReviewSimpleResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,6 +22,8 @@ import java.time.LocalDateTime;
 @RequestMapping("/api/reviews")
 public class ReviewController {
 
+    private final ReviewService reviewService;
+
     @Operation(
             summary = "동행 신청을 담당한 도우미의 리뷰 조회",
             description = "특정 동행 신청을 담당한 도우미의 리뷰를 조회합니다.")
@@ -31,12 +34,14 @@ public class ReviewController {
     public Response<ReviewSimpleResponse> getReviewAsRecruit(@PathVariable Long recruitId) {
         // TODO :: 신청 ID를 받아, 해당 신청을 담당한 도우미의 리뷰를 검색
 
-        ReviewSimpleResponse mock = ReviewSimpleResponse.builder()
-                .satisfactionLevel(SatisfactionLevel.GOOD)
-                .createdAt(LocalDateTime.now().minusDays(30))
-                .shortComment("약간 아쉽지만 좋았어요! 또 이용하고 싶습니다!")
-                .build();
+        return Response.from(reviewService.getReviewSimpleByRecruitId(recruitId));
 
-        return Response.from(mock);
+//        ReviewSimpleResponse mock = ReviewSimpleResponse.builder()
+//                .satisfactionLevel(SatisfactionLevel.GOOD)
+//                .createdAt(LocalDateTime.now().minusDays(30))
+//                .shortComment("약간 아쉽지만 좋았어요! 또 이용하고 싶습니다!")
+//                .build();
+//
+//        return Response.from(mock);
     }
 }
