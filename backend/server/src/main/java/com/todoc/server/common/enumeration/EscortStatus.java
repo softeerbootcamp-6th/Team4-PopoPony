b55a94b5ddb73bad05b4c0779745239a70b06c2c
seonghooni <lsh3717@naver.com>
2025-08-07 11:20:44 +0900

    Refactor: 각각의 Enum 클래스마다 String -> Enum으로 변환해주는 from 함수 추가 - 해당하는 Enum이 없을 경우 Optional<null>을 return

--- backend/server/src/main/java/com/todoc/server/common/enumeration/EscortStatus.java
+++ backend/server/src/main/java/com/todoc/server/common/enumeration/EscortStatus.java
@@ -2,6 +2,9 @@ package com.todoc.server.common.enumeration;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @Getter
 public enum EscortStatus {
     PREPARING("동행 준비"),
@@ -16,5 +19,17 @@ public enum EscortStatus {
 
     EscortStatus(String label) {
         this.label = label;
+    }
+
+    /**
+     * 문자열 값을 EscortStatus enum으로 변환합니다.
+     *
+     * @param value 변환할 문자열 값
+     * @return 해당하는 EscortStatus enum이 존재하면 Optional로 반환, 없으면 Optional.empty()
+     */
+    public static Optional<EscortStatus> from(String value) {
+        return Arrays.stream(values())
+                .filter(v -> v.name().equalsIgnoreCase(value))
+                .findFirst();
     }
 }
