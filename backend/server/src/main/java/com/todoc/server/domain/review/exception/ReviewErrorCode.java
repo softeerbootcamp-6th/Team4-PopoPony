ac00b2a65872397ea2e93df71cb2860b28433796
seonghooni <lsh3717@naver.com>
2025-08-07 11:28:30 +0900

    Feat: 존재하지 않는 Enum으로 변경할 경우 처리할 수 있는 예외 추가

--- backend/server/src/main/java/com/todoc/server/domain/review/exception/ReviewErrorCode.java
+++ backend/server/src/main/java/com/todoc/server/domain/review/exception/ReviewErrorCode.java
@@ -11,7 +11,9 @@ public enum ReviewErrorCode implements ResponseCode {
 
     // 리뷰 01
     REVIEW_NOT_FOUND(160101, HttpStatus.NOT_FOUND.value(), "해당 리뷰를 찾을 수 없습니다."),
-    REVIEW_SATISFACTION_IN_VALID_(160102, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 만족도입니다."),
+
+    // 리뷰 만족도 02
+    REVIEW_SATISFACTION_IN_VALID_(160202, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 만족도입니다."),
     ;
 
     private final int code;
