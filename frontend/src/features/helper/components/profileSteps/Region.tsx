aaeba929943a68419c3f61fbd86c45c6f5d786f5
yjbae3974 <brianbae6@gmail.com>
2025-08-09 00:52:48 +0900

    FIX: 제작한 bottom sheet component 적용

--- frontend/src/features/helper/components/profileSteps/Region.tsx
+++ frontend/src/features/helper/components/profileSteps/Region.tsx
@@ -3,7 +3,8 @@ import { useFormContext } from 'react-hook-form';
 import { RegionFormSchema, type ProfileStepProps, REGION_OPTIONS } from '@helper/types';
 import { useFormValidation } from '@hooks';
 import { FormLayout } from '@layouts';
-import { LabeledSection, PhotoUpload, BottomSheet, Button } from '@components';
+import { LabeledSection, PhotoUpload, Button } from '@components';
+import { RegionBottomSheet } from '@helper/components';
 import { IcChevronDown } from '@assets/icons';
 
 const Region = ({ handleNextStep }: ProfileStepProps) => {
@@ -29,54 +30,22 @@ const Region = ({ handleNextStep }: ProfileStepProps) => {
           label='선호 활동 지역'
           isChecked={!fieldErrors.region && !!values.region}
           message={fieldErrors.region}>
-          <BottomSheet open={isBottomSheetOpen} onOpenChange={setIsBottomSheetOpen}>
-            <BottomSheet.Trigger asChild>
-              <button className='flex-between border-b-neutral-20 w-full border-b-2 pb-[0.8rem]'>
-                <p
-                  className={`title-20-medium ${
-                    values.region ? 'text-text-neutral-primary' : 'text-text-neutral-assistive'
-                  }`}>
-                  {REGION_OPTIONS.find((option) => option.value === values.region)?.value ||
-                    '지역 선택'}
-                </p>
-                <IcChevronDown
-                  className={`[&_path]:stroke-icon-neutral-secondary [&_path]:fill-icon-neutral-secondary h-[2.4rem] w-[2.4rem] ${
-                    isBottomSheetOpen ? 'rotate-180' : ''
-                  }`}
-                />
-              </button>
-            </BottomSheet.Trigger>
-            <BottomSheet.Content>
-              <BottomSheet.Header>
-                <BottomSheet.Title>지역을 선택해주세요</BottomSheet.Title>
-              </BottomSheet.Header>
-              <div className='grid grid-cols-3 gap-x-[0.8rem] gap-y-[1.2rem] p-[2rem]'>
-                {REGION_OPTIONS.map((option, index) => {
-                  const uniqueKey = getUniqueKey(option.value, index);
-
-                  return (
-                    <div key={uniqueKey} className='w-full'>
-                      <input
-                        type='radio'
-                        id={uniqueKey}
-                        value={option.value}
-                        className='peer hidden'
-                        onClick={() => {
-                          setIsBottomSheetOpen(false);
-                        }}
-                        {...register('region')}
-                      />
-                      <label
-                        htmlFor={uniqueKey}
-                        className='border-neutral-20 peer-checked:border-mint-60 peer-checked:bg-mint-5 peer-checked:text-mint-70 flex-center body1-16-medium text-neutral-90 h-[4.8rem] w-full cursor-pointer rounded-[0.6rem] border'>
-                        {option.label}
-                      </label>
-                    </div>
-                  );
-                })}
-              </div>
-            </BottomSheet.Content>
-          </BottomSheet>
+          <RegionBottomSheet name='region'>
+            <button className='flex-between border-b-neutral-20 w-full border-b-2 pb-[0.8rem]'>
+              <p
+                className={`title-20-medium ${
+                  values.region ? 'text-text-neutral-primary' : 'text-text-neutral-assistive'
+                }`}>
+                {REGION_OPTIONS.find((option) => option.value === values.region)?.value ||
+                  '지역 선택'}
+              </p>
+              <IcChevronDown
+                className={`[&_path]:stroke-icon-neutral-secondary [&_path]:fill-icon-neutral-secondary h-[2.4rem] w-[2.4rem] ${
+                  isBottomSheetOpen ? 'rotate-180' : ''
+                }`}
+              />
+            </button>
+          </RegionBottomSheet>
         </LabeledSection>
       </FormLayout.Content>
       <FormLayout.Footer>
