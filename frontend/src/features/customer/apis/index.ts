e8963e6a4e0cfb41cb7ad400e32e635fa29ff291
TaeSeung Yoo <remicon99@gmail.com>
2025-08-08 11:39:23 +0900

    Feat: customer 도메인 API 함수 생성

--- frontend/src/features/customer/apis/index.ts
+++ frontend/src/features/customer/apis/index.ts
@@ -1,4 +1,17 @@
 import getRecruitsCustomer from './getRecruitsCustomer';
+import getRecruitById from './getRecruitById';
+import getPaymentById from './getPaymentById';
+import postRecruit from './postRecruit';
+import patchCancelRecruit from './patchCancelRecruit';
+
+// TODO: 추후 삭제
 import { searchRoute } from './search';
 
-export { getRecruitsCustomer, searchRoute };
+export {
+  getRecruitsCustomer,
+  getRecruitById,
+  getPaymentById,
+  postRecruit,
+  patchCancelRecruit,
+  searchRoute,
+};
