2a527543b2b8b7c9e150117cf07904531cc0d396
jaehyeonchoi <413cjh22@gmail.com>
2025-08-09 16:09:12 +0900

    Feat: HelperProfileAreaInvalidException - 도우미 프로필에 유효하지 않은 성별이 입력된 경우의 커스텀 예외 정의

--- backend/server/src/main/java/com/todoc/server/domain/helper/exception/HelperErrorCode.java
+++ backend/server/src/main/java/com/todoc/server/domain/helper/exception/HelperErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum HelperErrorCode implements ResponseCode {
-    HELPER_NOT_FOUND(140101, HttpStatus.NOT_FOUND.value(), "해당 도우미를 찾을 수 없습니다.");
+    HELPER_PROFILE_NOT_FOUND(140101, HttpStatus.NOT_FOUND.value(), "해당 도우미를 찾을 수 없습니다."),
+    HELPER_PROFILE_AREA_INVALID(140102, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 지역명입니다.");
 
     private final int code;
     private final int status;
