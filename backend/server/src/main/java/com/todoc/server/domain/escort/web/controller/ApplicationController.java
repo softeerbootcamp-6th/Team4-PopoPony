9a347202031d256aa51f693f207846eeaa9a7502
seonghooni <lsh3717@naver.com>
2025-08-09 22:41:22 +0900

    Feat: 동행 지원하기, 동행 지원 취소하기 Controller 코드 작성

--- backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/ApplicationController.java
+++ backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/ApplicationController.java
@@ -75,4 +75,34 @@ public class ApplicationController {
 
         return Response.from();
     }
+
+    @Operation(
+        summary = "일감 지원하기",
+        description = "로그인한 도우미가 동행에 지원합니다. recruitId를 통해 지원할 일감을 선택합니다.")
+    @ApiResponse(
+        responseCode = "200",
+        description = "동행 지원 성공")
+    @PostMapping("/recruits/{recruitId}")
+    public Response<Void> applyApplicaitonToRecruit(@PathVariable Long recruitId) {
+        // TODO :: Recruit ID를 받아, 해당 일감에 지원
+
+        // applicationFacadeService.applyApplicaitonToRecruit(recruitId);
+
+        return Response.from();
+    }
+
+    @Operation(
+        summary = "일감 취소하기",
+        description = "로그인한 도우미가 일감을 취소합니다. recruitId를 통해 취소할 일감을 선택합니다.")
+    @ApiResponse(
+        responseCode = "200",
+        description = "동행 지원 취소 성공")
+    @PatchMapping("/recruits/{recruitId}")
+    public Response<Void> cancelApplicationToRecruit(@PathVariable Long recruitId) {
+        // TODO :: Recruit ID를 받아, 해당 일감에 지원
+
+        // applicationFacadeService.cancelApplicationToRecruit(recruitId);
+
+        return Response.from();
+    }
 }
