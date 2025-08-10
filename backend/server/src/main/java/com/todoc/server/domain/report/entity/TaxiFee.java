f1bed076bfbd0b5bd588b6e7cebffc052d3a7e05
jaehyeonchoi <413cjh22@gmail.com>
2025-08-10 17:48:41 +0900

    Feat: TaxiFee - Builder 패턴 적용

--- backend/server/src/main/java/com/todoc/server/domain/report/entity/TaxiFee.java
+++ backend/server/src/main/java/com/todoc/server/domain/report/entity/TaxiFee.java
@@ -2,13 +2,16 @@ package com.todoc.server.domain.report.entity;
 
 import com.todoc.server.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @SQLRestriction("deleted_at is NULL")
 public class TaxiFee extends BaseEntity {
     @Id
@@ -32,4 +35,13 @@ public class TaxiFee extends BaseEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "return_receipt_image")
     private TaxiReceiptImage returnReceiptImage;
+
+    @Builder
+    public TaxiFee(Integer departureFee, TaxiReceiptImage departureReceiptImage,
+                   Integer returnFee, TaxiReceiptImage returnReceiptImage) {
+        this.departureFee = departureFee;
+        this.departureReceiptImage = departureReceiptImage;
+        this.returnFee = returnFee;
+        this.returnReceiptImage = returnReceiptImage;
+    }
 }
