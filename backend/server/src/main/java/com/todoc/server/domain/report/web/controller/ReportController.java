e9a90a986136685919c1b6eba78f32c3b49c8d17
seonghooni <lsh3717@naver.com>
2025-08-12 01:45:43 +0900

    Refactor: ReportController 및 서비스 연결 누락 추가

--- backend/server/src/main/java/com/todoc/server/domain/report/web/controller/ReportController.java
+++ backend/server/src/main/java/com/todoc/server/domain/report/web/controller/ReportController.java
@@ -1,8 +1,14 @@
 package com.todoc.server.domain.report.web.controller;
 
 import com.todoc.server.common.response.Response;
+import com.todoc.server.domain.auth.service.SessionAuth;
+import com.todoc.server.domain.auth.web.LoginUser;
+import com.todoc.server.domain.report.service.ReportFacadeService;
 import com.todoc.server.domain.report.service.ReportService;
+import com.todoc.server.domain.report.web.dto.request.ReportCreateRequest;
 import com.todoc.server.domain.report.web.dto.response.ReportDetailResponse;
+import com.todoc.server.domain.review.service.ReviewFacadeService;
+import com.todoc.server.domain.review.web.dto.request.ReviewCreateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,6 +25,7 @@ import java.time.LocalTime;
 public class ReportController {
 
     private final ReportService reportService;
+    private final ReportFacadeService reportFacadeService;
 
     @Operation(
             summary = "동행 신청에 대한 리포트 상세 정보 조회",
@@ -28,26 +35,8 @@ public class ReportController {
             description = "리포트 상세 정보 조회 성공")
     @GetMapping("recruits/{recruitId}")
     public Response<ReportDetailResponse> getReportAsRecruit(@PathVariable Long recruitId) {
-        // TODO :: 신청 ID를 받아, 해당 신청에 대한 리포트를 검색
-        // '리포트 작성 중' 상태에 대한 처리 필요
-        // 결제 정보까지 포함하여 전달 필요
 
-//        return Response.from(reportService.getReportDetailByRecruitId(recruitId));
-
-        ReportDetailResponse mock = ReportDetailResponse.builder()
-                .reportId(1L)
-                .actualMeetingTime(LocalTime.NOON)
-                .actualReturnTime(LocalTime.MIDNIGHT)
-                .extraMinutes(30)
-                .hasNextAppointment(true)
-                .nextAppointmentTime(LocalDateTime.now().plusDays(30))
-                .description("오늘 치료 잘 받으셨고, 약은 가방 안에 넣어드렸습니다.")
-                .baseFee(30000)
-                .taxiFee(50000)
-                .extraTimeFee(20000)
-                .build();
-
-        return Response.from(mock);
+        return Response.from(reportService.getReportDetailByRecruitId(recruitId));
     }
 
     @Operation(
@@ -57,14 +46,9 @@ public class ReportController {
             responseCode = "200",
             description = "리포트 등록에 필요한 기본값 조회 성공")
     @GetMapping("recruits/{recruitId}/default")
-    public Response<Void> getReportDefaultValueOnRecruit(@PathVariable Long recruitId) {
-        // TODO :: 신청 ID를 받아, 해당 신청에 대한 리포트를 작성하기 위해 필요한 기본값 조회
+    public Response<ReportDetailResponse> getReportDefaultValueOnRecruit(@PathVariable Long recruitId) {
 
-//        return Response.from(reportService.getReportDetailByRecruitId(recruitId));
-
-
-
-        return Response.from();
+        return Response.from(reportService.getReportDetailByRecruitId(recruitId));
     }
 
     @Operation(
@@ -74,12 +58,9 @@ public class ReportController {
             responseCode = "200",
             description = "리포트 등록 성공")
     @PostMapping("recruits/{recruitId}")
-    public Response<Void> createReportOnRecruit(@PathVariable Long recruitId) {
-        // TODO :: 신청 ID를 받아, 해당 신청에 대한 리포트를 작성
+    public Response<Void> createReportOnRecruit(@LoginUser SessionAuth auth, @RequestBody ReportCreateRequest request) {
 
-//        return Response.from(reportService.getReportDetailByRecruitId(recruitId));
-
-
+        reportFacadeService.createReport(request, auth.id());
 
         return Response.from();
     }
