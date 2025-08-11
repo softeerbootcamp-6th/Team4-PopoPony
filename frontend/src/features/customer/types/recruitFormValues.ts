4772100f316e86a00268b038c3849fe11fec113a
yjbae3974 <brianbae6@gmail.com>
2025-08-11 18:01:50 +0900

    FIX: 스키마 이동 및 타입 변경

--- frontend/src/features/customer/types/recruitFormValues.ts
+++ frontend/src/features/customer/types/recruitFormValues.ts
@@ -1,56 +1,152 @@
+import { z } from 'zod';
+import { imageSchema } from '@types';
 // 폼 단계별 타입 정의
-export interface ProfileFormValues {
-  imageUrl: string; // 프로필 이미지
-  name: string; // 환자 이름
-  age: number;
-  gender: '남자' | '여자';
-  phoneNumber: string;
-}
 
-export interface ConditionFormValues {
-  needsHelping: boolean;
-  usesWheelchair: boolean;
-}
+export const profileSchema = z.object({
+  name: z.string().min(2, { message: '올바른 이름을 입력해주세요' }),
+  age: z
+    .string()
+    .min(1, { message: '나이를 입력해주세요' })
+    .refine(
+      (val) => {
+        const num = Number(val);
+        return !isNaN(num) && num >= 1 && num <= 150;
+      },
+      { message: '올바른 나이를 입력해주세요' }
+    ),
+  phoneNumber: z.string().min(12, { message: '숫자만 입력해주세요' }),
+  gender: z.enum(['남자', '여자'], { message: '성별을 선택해주세요' }),
+  imageUrl: imageSchema,
+});
 
-export interface CommunicationFormValues {
-  hasCognitiveIssue: boolean;
-  cognitiveIssueDetail?: string[]; // 체크박스 다중 선택
-  hasCommunicationIssue: boolean;
-  communicationIssueDetail?: string; // 단순 텍스트
-}
+export type ProfileFormValues = z.infer<typeof profileSchema>;
 
-export interface TimeFormValues {
-  escortDate: string;
-  estimatedMeetingTime: string;
-  estimatedReturnTime: string;
-  escortDuration: number; // 얘는 백엔드에서 보낼 때
-}
+export const conditionSchema = z.object({
+  needsHelping: z.boolean(),
+  usesWheelchair: z.boolean(),
+});
 
-export interface LocationDetail {
-  placeName: string;
-  upperAddrName: string;
-  middleAddrName: string;
-  lowerAddrName: string;
-  firstAddrNo: string;
-  secondAddrNo: string;
-  roadName: string;
-  firstBuildingNo: string;
-  secondBuildingNo: string;
-  detailAddress: string;
-  longitude: number;
-  latitude: number;
-}
+export type ConditionFormValues = z.infer<typeof conditionSchema>;
 
-export interface RouteFormValues {
-  meetingLocationDetail: LocationDetail;
-  destinationDetail: LocationDetail;
-  returnLocationDetail: LocationDetail;
-}
+export const CognitiveSchema = z
+  .object({
+    hasCognitiveIssue: z.boolean(),
+    cognitiveIssueDetail: z.array(z.string()).optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.hasCognitiveIssue) {
+        return data.cognitiveIssueDetail && data.cognitiveIssueDetail.length > 0;
+      }
+      return true;
+    },
+    {
+      message: '구체적인 문제점을 선택해주세요',
+      path: ['cognitiveIssueDetail'],
+    }
+  );
+export const CommunicationSchema = z
+  .object({
+    hasCommunicationIssue: z.boolean(),
+    communicationIssueDetail: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.hasCommunicationIssue) {
+        return data.communicationIssueDetail && data.communicationIssueDetail.length >= 10;
+      }
+      return true;
+    },
+    {
+      message: '10자 이상 입력해주세요',
+      path: ['communicationIssueDetail'],
+    }
+  );
 
-export interface RequestFormValues {
-  purpose: string;
-  extraRequest: string;
-}
+export const IntegratedCommSchema = z.intersection(CognitiveSchema, CommunicationSchema);
+
+export type CommunicationFormValues = z.infer<typeof IntegratedCommSchema>;
+
+export const dateSchema = z.object({
+  escortDate: z
+    .string()
+    .min(1, { message: '날짜를 선택해주세요' })
+    .refine(
+      (date) => {
+        const today = new Date();
+        today.setHours(23, 59, 59, 999);
+        const selectedDate = new Date(date);
+        return selectedDate > today;
+      },
+      { message: '오늘 이후의 날짜를 선택해주세요' }
+    ),
+});
+
+export const timeSchema = z
+  .object({
+    estimatedMeetingTime: z.string().min(1, { message: '시작 시간을 선택해주세요' }),
+    estimatedReturnTime: z.string().min(1, { message: '종료 시간을 선택해주세요' }),
+    escortDuration: z.number(),
+  })
+  .refine(
+    (data) => {
+      const startTime = new Date(`2000-01-01T${data.estimatedMeetingTime}`);
+      const endTime = new Date(`2000-01-01T${data.estimatedReturnTime}`);
+      return startTime < endTime;
+    },
+    {
+      message: '시작 시간이 종료 시간보다 늦습니다.',
+      path: ['estimatedReturnTime'],
+    }
+  )
+  .refine(
+    (data) => {
+      const startTime = new Date(`2000-01-01T${data.estimatedMeetingTime}`);
+      const endTime = new Date(`2000-01-01T${data.estimatedReturnTime}`);
+      const diffInMinutes = (endTime.getTime() - startTime.getTime()) / (1000 * 60);
+      return diffInMinutes >= 120;
+    },
+    {
+      message: '최소 2시간 이상 예약해주세요',
+      path: ['escortDuration'],
+    }
+  );
+
+export const timeAndDateSchema = z.intersection(timeSchema, dateSchema);
+
+export type TimeFormValues = z.infer<typeof timeAndDateSchema>;
+
+export const locationDetailSchema = z.object({
+  placeName: z.string(),
+  upperAddrName: z.string(),
+  middleAddrName: z.string(),
+  lowerAddrName: z.string(),
+  firstAddrNo: z.string(),
+  secondAddrNo: z.string(),
+  roadName: z.string(),
+  firstBuildingNo: z.string(),
+  secondBuildingNo: z.string().optional(),
+  detailAddress: z.string().min(1, { message: '필수 입력 항목입니다.' }),
+  longitude: z.number(),
+  latitude: z.number(),
+});
+
+export const routeFormSchema = z.object({
+  meetingLocationDetail: locationDetailSchema,
+  destinationDetail: locationDetailSchema,
+  returnLocationDetail: locationDetailSchema,
+  isMeetingLocationSameAsDestination: z.boolean(),
+});
+
+export type LocationDetail = z.infer<typeof locationDetailSchema>;
+
+export type RouteFormValues = z.infer<typeof routeFormSchema>;
+
+export const requestFormSchema = z.object({
+  purpose: z.string().min(1, '요청 사항을 입력해주세요'),
+  extraRequest: z.string().optional(),
+});
+export type RequestFormValues = z.infer<typeof requestFormSchema>;
 
 // 전체 폼 타입 (백엔드 스키마와 일치)
 export interface RecruitFormValues {
