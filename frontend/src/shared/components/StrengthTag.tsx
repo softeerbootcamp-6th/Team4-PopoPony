f6b9d396811515a26607c343a0ddc2c6ad9ed11f
TaeSeung Yoo <remicon99@gmail.com>
2025-08-11 15:20:51 +0900

    Fix: StrengthTag type 서버 상태에 맞게 수정

--- frontend/src/shared/components/StrengthTag.tsx
+++ frontend/src/shared/components/StrengthTag.tsx
@@ -1,17 +1,18 @@
 import { IcRecognize, IcShoes, IcWheelchair } from '@icons';
+import type { escortStrength } from '@types';
 import Tag from './Tag';
 
 interface Props {
-  type: 'support' | 'wheelchair' | 'care';
+  type: escortStrength;
 }
 
 const StrengthTag = ({ type }: Props) => {
-  const typeMap = {
-    support: { icon: <IcShoes />, label: '안전한 부축' },
-    wheelchair: { icon: <IcWheelchair />, label: '휠체어 이동' },
-    care: { icon: <IcRecognize />, label: '인지장애 케어' },
+  const iconMap = {
+    '안전한 부축': <IcShoes />,
+    '휠체어 이동': <IcWheelchair />,
+    '인지장애 케어': <IcRecognize />,
   };
-  return <Tag icon={typeMap[type].icon} text={typeMap[type].label} />;
+  return <Tag icon={iconMap[type]} text={type} />;
 };
 
 export default StrengthTag;
