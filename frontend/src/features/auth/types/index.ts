aa16d1349a9bc58ba779da12d9f15c6d94f4df8e
TaeSeung Yoo <remicon99@gmail.com>
2025-08-12 18:30:55 +0900

    Fix: 필요없는 코드 삭제

--- frontend/src/features/auth/types/index.ts
+++ frontend/src/features/auth/types/index.ts
@@ -2,13 +2,3 @@ export interface LoginFormValues {
   loginId: string;
   password: string;
 }
-
-export interface LoginResponse {
-  accessToken: string;
-  refreshToken: string;
-  user: {
-    id: string;
-    name: string;
-    role: 'CUSTOMER' | 'HELPER';
-  };
-}
