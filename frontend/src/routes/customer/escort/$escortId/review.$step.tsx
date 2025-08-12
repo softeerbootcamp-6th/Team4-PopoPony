00795133317bfae4e4b16abffed81fa8b3bd35ce
yjbae3974 <brianbae6@gmail.com>
2025-08-12 14:59:20 +0900

    Feat: 페이지 생성

--- frontend/src/routes/customer/escort/$escortId/review.$step.tsx
+++ frontend/src/routes/customer/escort/$escortId/review.$step.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useRouter } from '@tanstack/react-router';
+import { useFunnel } from '@hooks';
+import { PageLayout } from '@layout';
+import { ProgressBar } from '@components';
+import { FormProvider, useForm } from 'react-hook-form';
 
 export const Route = createFileRoute('/customer/escort/$escortId/review/$step')({
   component: RouteComponent,
 });
+
+const stepList = ['summary', 'detail', 'review']
 
 function RouteComponent() {
   return <div>Hello "/customer/escort/$escortId/review/$reviewStep"!</div>;
