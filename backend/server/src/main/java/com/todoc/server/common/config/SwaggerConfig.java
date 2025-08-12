67e2c25e2c64ccfc0362f343155419a7a03b29aa
jaehyeonchoi <413cjh22@gmail.com>
2025-08-12 19:01:40 +0900

    Feat: SwaggerConfig - 쿠키를 통한 Authorization 구현

--- backend/server/src/main/java/com/todoc/server/common/config/SwaggerConfig.java
+++ backend/server/src/main/java/com/todoc/server/common/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package com.todoc.server.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import java.util.List;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,14 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        SecurityScheme auth = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.COOKIE).name("JSESSIONID");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("basicAuth");
+
         return new OpenAPI()
-            .servers(List.of(
-                new Server().url("/").description("default server")));
+                .components(new Components().addSecuritySchemes("basicAuth", auth))
+                .addSecurityItem(securityRequirement)
+                .info(new Info().title("PopoPony todoc"));
     }
 }
