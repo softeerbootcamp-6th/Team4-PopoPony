33c8631a8d5d8d1aaf184c128e2a3bf1f9e798c5
TaeSeung Yoo <remicon99@gmail.com>
2025-08-13 22:46:47 +0900

    Feat: 동행 리포트 시간 입력 퍼블리싱

--- frontend/src/features/helper/components/reportStep/Time.tsx
+++ frontend/src/features/helper/components/reportStep/Time.tsx
@@ -1,7 +1,47 @@
+import { Button, FormInput, LabeledSection } from '@components';
+import { FormLayout } from '@layouts';
 import type { FunnelStepProps } from '@types';
 
 const Time = ({ handleNextStep }: FunnelStepProps) => {
-  return <div>Time</div>;
+  return (
+    <FormLayout>
+      <FormLayout.Content>
+        <FormLayout.TitleWrapper>
+          <FormLayout.Title>
+            <strong className='text-text-mint-on-primary'>총 2시간 56분</strong>
+            <br />
+            동행을 마무리하셨어요!
+          </FormLayout.Title>
+          <FormLayout.SubTitle>실제 진행 시간과 다르다면 수정해주세요.</FormLayout.SubTitle>
+        </FormLayout.TitleWrapper>
+        <div className='flex gap-[1.2rem]'>
+          <LabeledSection label='시작 시간' isChecked={true}>
+            <FormInput
+              type='time'
+              size='M'
+              name='estimatedMeetingTime'
+              placeholder='시작 시간 선택'
+              validation={() => {}}
+            />
+          </LabeledSection>
+          <LabeledSection label='종료 시간' isChecked={true}>
+            <FormInput
+              type='time'
+              size='M'
+              name='estimatedReturnTime'
+              placeholder='종료 시간 선택'
+              validation={() => {}}
+            />
+          </LabeledSection>
+        </div>
+      </FormLayout.Content>
+      <FormLayout.Footer>
+        <Button onClick={handleNextStep} disabled={false}>
+          네, 맞아요
+        </Button>
+      </FormLayout.Footer>
+    </FormLayout>
+  );
 };
 
 export default Time;
