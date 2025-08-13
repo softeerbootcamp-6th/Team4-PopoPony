bd66c92b6e9230eab57bf80f66220522f7727754
yjbae3974 <brianbae6@gmail.com>
2025-08-13 22:47:52 +0900

    Fix: 이미지 타입 임시 변경

--- frontend/src/shared/types/imageType.ts
+++ frontend/src/shared/types/imageType.ts
@@ -2,8 +2,6 @@ import { z } from 'zod';
 
 export const imageSchema = z.object({
   imageUrl: z.string().min(1, { message: '이미지를 선택해주세요' }),
-  s3_key: z.string().optional(),
-  md5: z.string().optional(),
 });
 
 export type ImageType = z.infer<typeof imageSchema>;
