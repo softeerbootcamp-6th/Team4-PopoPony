8344463772250e41fa59a0cb88d0a02271f350ff
jaehyeonchoi <413cjh22@gmail.com>
2025-08-13 14:56:31 +0900

    Fix: RecruitController - 요청 DTO에 RequestBody 어노테이션 추가

--- backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/RecruitController.java
+++ backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/RecruitController.java
@@ -13,7 +13,6 @@ import com.todoc.server.domain.escort.web.dto.response.*;
 import com.todoc.server.domain.route.web.dto.response.LocationInfoSimpleResponse;
 import com.todoc.server.domain.route.web.dto.response.RouteSimpleResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.LinkedHashMap;
