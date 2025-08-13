d784384b5252eeac37c6fa087c639f36986ab80e
yjbae3974 <brianbae6@gmail.com>
2025-08-13 22:45:20 +0900

    Chore: 타입 오류 수정

--- frontend/src/shared/utils/parseType.ts
+++ frontend/src/shared/utils/parseType.ts
@@ -2,6 +2,7 @@ export const parseStringToBoolean = (value: string): boolean => {
   return value === 'true';
 };
 
-export const booleanToString = (value: boolean): string => {
+export const booleanToString = (value: boolean | undefined): string => {
+  if (value === undefined) return 'false';
   return value ? 'true' : 'false';
 };
