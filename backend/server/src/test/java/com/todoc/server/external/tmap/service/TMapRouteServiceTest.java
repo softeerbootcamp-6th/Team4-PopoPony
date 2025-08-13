75dd1ec6861504de18035d9998118f22b0d86cb4
seonghooni <lsh3717@naver.com>
2025-08-14 00:33:29 +0900

    Feat: TMap 연동서비스 단위테스트는 주석 처리

--- backend/server/src/test/java/com/todoc/server/external/tmap/service/TMapRouteServiceTest.java
+++ backend/server/src/test/java/com/todoc/server/external/tmap/service/TMapRouteServiceTest.java
@@ -1,43 +1,48 @@
 package com.todoc.server.external.tmap.service;
 
+import com.todoc.server.IntegrationMockConfig;
 import com.todoc.server.external.tmap.web.dto.RouteExternalRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * TMapRouteServiceTest는 TMap API를 통해 경로 조회 기능을 테스트하는 클래스입니다.
+ * 주석을 풀 경우, 매 테스트 실행마다 TMap API를 호출합니다.
+ */
+@Import(IntegrationMockConfig.class)
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
 class TMapRouteServiceTest {
 
-    @Autowired
-    private TMapRouteService tMapRouteService;
 
-    @Autowired
-    private RestClient restClient;
-
-    @Test
-    @DisplayName("TMap API 경로 조회 테스트")
-    void getRouteTest() {
-        RouteExternalRequest request = RouteExternalRequest.builder()
-                .startX("126.9783882") // 서울시청 경도
-                .startY("37.5666103")  // 서울시청 위도
-                .endX("129.042628")    // 예: 사직야구장 경도
-                .endY("35.194568")     // 예: 사직야구장 위도
-                .reqCoordType("WGS84GEO")
-                .resCoordType("WGS84GEO")
-                .startName("서울시청")
-                .endName("사직야구장")
-                .build();
-
-        System.out.println(tMapRouteService.getRoute(request));
-    }
+//    @Autowired
+//    private TMapRouteService tMapRouteService;
+//
+//    @Autowired
+//    private RestClient restClient;
+//
+//    @Test
+//    @DisplayName("TMap API 경로 조회 테스트")
+//    void getRouteTest() {
+//        RouteExternalRequest request = RouteExternalRequest.builder()
+//                .startX("126.9783882")
+//                .startY("37.5666103")
+//                .endX("126.966666")
+//                .endY("37.5666103")
+//                .reqCoordType("WGS84GEO")
+//                .resCoordType("WGS84GEO")
+//                .startName("서울시청")
+//                .endName("사직야구장")
+//                .build();
+//
+//        System.out.println(tMapRouteService.getRoute(request));
+//    }
 
 }
