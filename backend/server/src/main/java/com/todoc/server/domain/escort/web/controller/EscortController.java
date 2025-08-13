d32e7941d21de6a139d90c9dc600b2551255e407
jaehyeonchoi <413cjh22@gmail.com>
2025-08-13 14:29:22 +0900

    Feat: EscortController - 동행 진행 상태 이동에 대한 핸들러 메서드 정의

---
+++ backend/server/src/main/java/com/todoc/server/domain/escort/web/controller/EscortController.java
@@ -0,0 +1,32 @@
+package com.todoc.server.domain.escort.web.controller;
+
+import com.todoc.server.common.response.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "escorts", description = "동행 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/escorts")
+public class EscortController {
+
+    @Operation(
+            summary = "동행 다음 단계로 이동하기",
+            description = "로그인한 도우미가 동행(일감)의 다음 단계로 이동합니다. recruitId를 통해 진행할 동행(일감)을 선택합니다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "동행 진행 취소 성공")
+    @PatchMapping("/{recruitId}")
+    public Response<Void> cancelApplicationToRecruit(@PathVariable Long recruitId) {
+
+        // applicationFacadeService.cancelApplicationToRecruit(applicationId);
+
+        return Response.from();
+    }
+}
