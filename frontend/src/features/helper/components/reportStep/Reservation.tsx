9aa5d13fba17ec8eb9e4b959000bce7e218010d9
TaeSeung Yoo <remicon99@gmail.com>
2025-08-14 01:27:26 +0900

    Feat: 동행 리포트 다음 예약 페이지 퍼블리싱

--- frontend/src/features/helper/components/reportStep/Reservation.tsx
+++ frontend/src/features/helper/components/reportStep/Reservation.tsx
@@ -1,7 +1,43 @@
+import { Checkbox, FormInput } from '@components';
+import { FormLayout } from '@layouts';
 import type { FunnelStepProps } from '@types';
 
 const Reservation = ({ handleNextStep }: FunnelStepProps) => {
-  return <div>Reservation</div>;
+  return (
+    <FormLayout>
+      <FormLayout.Content>
+        <FormLayout.TitleWrapper>
+          <FormLayout.Title>
+            다음 진료의
+            <br />
+            예약 일정을 알려주세요
+          </FormLayout.Title>
+          <FormLayout.SubTitle>
+            <Checkbox label='다음 진료를 예약하지 않았어요' checked={false} />
+          </FormLayout.SubTitle>
+        </FormLayout.TitleWrapper>
+        <div className='flex gap-[1.2rem]'>
+          <FormInput
+            type='date'
+            size='M'
+            name='escortDate'
+            placeholder='날짜 선택'
+            validation={() => {}}
+          />
+          <FormInput
+            type='time'
+            size='M'
+            name='estimatedReturnTime'
+            placeholder='종료 시간 선택'
+            validation={() => {}}
+          />
+        </div>
+      </FormLayout.Content>
+      <FormLayout.Footer>
+        <FormLayout.FooterPrevNext handleClickNext={handleNextStep} disabled={false} />
+      </FormLayout.Footer>
+    </FormLayout>
+  );
 };
 
 export default Reservation;
