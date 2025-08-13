31e881f33de55866941f184920a4cf9bcc741514
jaehyeonchoi <413cjh22@gmail.com>
2025-08-13 18:06:56 +0900

    Refactor: PresignBatchRequest - 불필요한 import문 제거

--- backend/server/src/main/java/com/todoc/server/domain/image/web/dto/request/PresignBatchRequest.java
+++ backend/server/src/main/java/com/todoc/server/domain/image/web/dto/request/PresignBatchRequest.java
@@ -1,6 +1,5 @@
 package com.todoc.server.domain.image.web.dto.request;
 
-import com.todoc.server.common.enumeration.S3Prefix;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
