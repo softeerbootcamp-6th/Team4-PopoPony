38f69c98cb0733142fc776794b2b5a5cfd9b98c6
TaeSeung Yoo <remicon99@gmail.com>
2025-08-14 18:24:50 +0900

    Feat: 동행 리포트 택시 요금 입력 페이지 추가

--- frontend/src/features/helper/components/reportStep/Taxi.tsx
+++ frontend/src/features/helper/components/reportStep/Taxi.tsx
@@ -1,9 +1,26 @@
-import { FormInput } from '@components';
 import { FormLayout } from '@layouts';
 import type { FunnelStepProps } from '@types';
 import { ReceiptImageUpload } from '@helper/components';
+import { useFormContext, useWatch } from 'react-hook-form';
+import { formatValue } from '@utils';
 
 const Taxi = ({ handleNextStep }: FunnelStepProps) => {
+  const { register, control, watch } = useFormContext();
+
+  const departureFee = useWatch({
+    control,
+    name: 'taxiFeeCreateRequest.departureFee',
+  });
+  const returnFee = useWatch({
+    control,
+    name: 'taxiFeeCreateRequest.returnFee',
+  });
+
+  const departureReceipt = watch('taxiFeeCreateRequest.departureReceipt');
+  const returnReceipt = watch('taxiFeeCreateRequest.returnReceipt');
+
+  const isValid = departureFee && returnFee && departureReceipt.s3Key && returnReceipt.s3Key;
+
   return (
     <FormLayout>
       <FormLayout.Content>
@@ -21,30 +38,50 @@ const Taxi = ({ handleNextStep }: FunnelStepProps) => {
         <div className='flex gap-[1.2rem]'>
           <div className='flex flex-col gap-[0.8rem]'>
             <span className='body2-14-medium text-text-neutral-secondary'>만남-병원 요금</span>
-            <FormInput
-              type='cost'
-              size='M'
-              name='departureFee'
-              placeholder='택시 요금'
-              validation={() => {}}
-            />
+            <div
+              className={`border-stroke-neutral-dark bg-background-default-white focus-within:border-stroke-mint focus-within:ring-stroke-mint/20 relative flex h-[5.1rem] w-full items-center rounded-[0.8rem] border px-[1.6rem] transition-[color,box-shadow] focus-within:ring-[0.3rem]`}>
+              <input
+                type='text'
+                className='body1-16-medium text-text-neutral-primary placeholder:text-text-neutral-assistive w-full min-w-0 flex-1 bg-transparent outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50'
+                placeholder='택시 요금'
+                {...register('taxiFeeCreateRequest.departureFee', {
+                  onChange: (e) => {
+                    const formattedValue = formatValue(e.target.value, 'cost');
+                    e.target.value = formattedValue;
+                  },
+                })}
+              />
+              <div className='body1-16-medium text-text-neutral-assistive ml-[0.8rem] select-none'>
+                원
+              </div>
+            </div>
             <ReceiptImageUpload
-              name='departureReceipt'
+              name='taxiFeeCreateRequest.departureReceipt'
               prefix='uploads/taxi'
               placeholder='만남-병원'
             />
           </div>
           <div className='flex flex-col gap-[0.8rem]'>
             <span className='body2-14-medium text-text-neutral-secondary'>병원-복귀 요금</span>
-            <FormInput
-              type='cost'
-              size='M'
-              name='returnFee'
-              placeholder='택시 요금'
-              validation={() => {}}
-            />
+            <div
+              className={`border-stroke-neutral-dark bg-background-default-white focus-within:border-stroke-mint focus-within:ring-stroke-mint/20 relative flex h-[5.1rem] w-full items-center rounded-[0.8rem] border px-[1.6rem] transition-[color,box-shadow] focus-within:ring-[0.3rem]`}>
+              <input
+                type='text'
+                className='body1-16-medium text-text-neutral-primary placeholder:text-text-neutral-assistive w-full min-w-0 flex-1 bg-transparent outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50'
+                placeholder='택시 요금'
+                {...register('taxiFeeCreateRequest.returnFee', {
+                  onChange: (e) => {
+                    const formattedValue = formatValue(e.target.value, 'cost');
+                    e.target.value = formattedValue;
+                  },
+                })}
+              />
+              <div className='body1-16-medium text-text-neutral-assistive ml-[0.8rem] select-none'>
+                원
+              </div>
+            </div>
             <ReceiptImageUpload
-              name='returnReceipt'
+              name='taxiFeeCreateRequest.returnReceipt'
               prefix='uploads/taxi'
               placeholder='병원-복귀'
             />
@@ -52,7 +89,7 @@ const Taxi = ({ handleNextStep }: FunnelStepProps) => {
         </div>
       </FormLayout.Content>
       <FormLayout.Footer>
-        <FormLayout.FooterPrevNext handleClickNext={handleNextStep} disabled={false} />
+        <FormLayout.FooterPrevNext handleClickNext={handleNextStep} disabled={!isValid} />
       </FormLayout.Footer>
     </FormLayout>
   );
