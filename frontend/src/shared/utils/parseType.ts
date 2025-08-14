f30bb04341e4631a7cdcab2075f4144546a8339f
yjbae3974 <brianbae6@gmail.com>
2025-08-14 13:26:04 +0900

    Feat: 타입 변경 유틸 함수 추가

--- frontend/src/shared/utils/parseType.ts
+++ frontend/src/shared/utils/parseType.ts
@@ -6,3 +6,11 @@ export const booleanToString = (value: boolean | undefined): string => {
   if (value === undefined) return 'false';
   return value ? 'true' : 'false';
 };
+
+export const stringToNumber = (value: string): number => {
+  return Number(value);
+};
+
+export const numberToString = (value: number): string => {
+  return String(value);
+};
