984b9f31b038b2eef8e2e14105cd8f0470bf033a
yjbae3974 <brianbae6@gmail.com>
2025-08-15 19:18:10 +0900

    Feat: 도우미 화면 api 연동

--- frontend/src/features/customer/components/tabs/HelperTab.tsx
+++ frontend/src/features/customer/components/tabs/HelperTab.tsx
@@ -3,17 +3,33 @@ import { HelperCard, HelperEmptyCard, HelperSelectInfoCard } from '@customer/com
 import { useParams, useNavigate } from '@tanstack/react-router';
 import { getApplicationListById } from '@customer/apis';
 
-const HelperTab = () => {
+const HelperTab = ({ status }: { status: string }) => {
   const navigate = useNavigate();
   const { escortId } = useParams({ from: '/customer/escort/$escortId/' });
   const { data: applicationList, isLoading } = getApplicationListById(Number(escortId));
-  console.log(applicationList);
 
-  const handleHelperCardClick = (helperId: number) => {
-    navigate({
-      to: '/customer/escort/$escortId/helper/$helperId',
-      params: { escortId: escortId, helperId: helperId.toString() },
-    });
+  const handleHelperCardClick = (helperId: number, escortId: string, applicationId: number) => {
+    if (status === '매칭중') {
+      navigate({
+        to: '/customer/escort/$escortId/$helperId/helper/$applicationId',
+        params: {
+          escortId: escortId,
+          helperId: helperId.toString(),
+          applicationId: applicationId.toString(),
+        },
+        search: { canSelect: 'true' },
+      });
+    } else {
+      navigate({
+        to: '/customer/escort/$escortId/$helperId/helper/$applicationId',
+        params: {
+          escortId: escortId,
+          helperId: helperId.toString(),
+          applicationId: applicationId.toString(),
+        },
+        search: { canSelect: 'false' },
+      });
+    }
   };
 
   if (isLoading) return <Spinner />;
@@ -24,12 +40,18 @@ const HelperTab = () => {
       applicationList.data &&
       applicationList.data.applicationList.length > 0 ? (
         <>
-          <HelperSelectInfoCard />
+          {status === '매칭중' && <HelperSelectInfoCard />}
           {applicationList.data.applicationList.map((application) => (
             <HelperCard
               key={application.helper.helperProfileId}
               helper={application.helper}
-              onClick={() => handleHelperCardClick(application.helper.helperProfileId)}
+              onClick={() =>
+                handleHelperCardClick(
+                  application.helper.helperProfileId,
+                  escortId,
+                  application.applicationId
+                )
+              }
             />
           ))}
         </>
