e59e6c02540032eaa2dc54ee8ab3e3566b974db3
TaeSeung Yoo <remicon99@gmail.com>
2025-08-15 20:17:59 +0900

    Feat: Spinner classname 인자 추가

--- frontend/src/shared/components/Spinner.tsx
+++ frontend/src/shared/components/Spinner.tsx
@@ -2,18 +2,21 @@ import { IcSpinner } from '@icons';
 
 interface Props {
   size?: string;
-  color?: 'black' | 'white';
+  color?: 'black' | 'white' | 'primary';
   isLoading?: boolean;
+  className?: string;
 }
 
-const Spinner = ({ size = '24', color = 'black', isLoading = false }: Props) => {
+const Spinner = ({ size = '24', color = 'black', isLoading = false, className }: Props) => {
   const colorStyle = {
     black: '[&_path]:fill-neutral-90',
     white: '[&_path]:fill-neutral-0',
+    primary: '[&_path]:fill-mint-50',
   };
 
   return (
-    <div className={`animate-spin items-center justify-center ${isLoading ? 'flex' : 'hidden'}`}>
+    <div
+      className={`animate-spin items-center justify-center ${isLoading ? 'flex' : 'hidden'} ${className}`}>
       <IcSpinner
         className={`${colorStyle[color]} [&_path]:fill-opacity-1`}
         width={size}
