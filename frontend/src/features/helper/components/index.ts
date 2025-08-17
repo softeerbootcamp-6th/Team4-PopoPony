e5744e82df2883c83e91365362692c94524c0569
yjbae3974 <brianbae6@gmail.com>
2025-08-18 00:59:53 +0900

    Feat: 도우미 동행 상세페이지 리포트, 지원내역 페이지 제작

--- frontend/src/features/helper/components/index.ts
+++ frontend/src/features/helper/components/index.ts
@@ -2,5 +2,7 @@ import Region from './profileSteps/Region';
 import Detail from './profileSteps/Detail';
 import RegionBottomSheet from './modal/RegionBottomSheet';
 import CertificateImageUploader from './form/CertificateImageUploader';
+import DetailTab from './tabs/DetailTab';
+import ReportTab from './tabs/ReportTab';
 
-export { Region, Detail, RegionBottomSheet, CertificateImageUploader };
+export { Region, Detail, RegionBottomSheet, CertificateImageUploader, DetailTab, ReportTab };
