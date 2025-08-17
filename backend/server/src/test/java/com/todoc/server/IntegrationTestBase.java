6e3d509bbab250ecdc5e603d7d9a2999e1e1058d
seonghooni <lsh3717@naver.com>
2025-08-17 22:44:46 +0900

    Chore: CI 에러 해결 반영

--- backend/server/src/test/java/com/todoc/server/IntegrationTestBase.java
+++ backend/server/src/test/java/com/todoc/server/IntegrationTestBase.java
@@ -2,12 +2,12 @@ package com.todoc.server;
 
 import com.todoc.server.domain.latestlocation.repository.LocationCacheRepository;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class IntegrationTestBase {
-    @MockBean
+    @MockitoBean
     LocationCacheRepository locationCacheRepository;
 }
