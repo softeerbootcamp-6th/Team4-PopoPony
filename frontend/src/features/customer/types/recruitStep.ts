7f67a1887c60ee973ab043b630eb03c6ee195d3a
TaeSeung Yoo <remicon99@gmail.com>
2025-08-17 19:24:17 +0900

    Feat: SearchParam 타입 지정 및 잘못된 경로 예외처리

--- frontend/src/features/customer/types/recruitStep.ts
+++ frontend/src/features/customer/types/recruitStep.ts
@@ -1,1 +1,10 @@
+import z from 'zod';
+
 export type RecruitStepProps = { handleNextStep?: () => void; handleBackStep?: () => void };
+
+const placeSchema = z.enum(['meeting', 'hospital', 'return']).optional();
+export type PlaceType = z.infer<typeof placeSchema>;
+
+export const recruitStepSearchSchema = z.object({
+  place: placeSchema,
+});
