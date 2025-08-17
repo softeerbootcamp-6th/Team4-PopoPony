a725c779b6f44d0aa307b121188969952240def2
yjbae3974 <brianbae6@gmail.com>
2025-08-17 20:29:30 +0900

    Feat: 스키마 기반 타입 추가

--- frontend/src/features/helper/types/schemaTypes.ts
+++ frontend/src/features/helper/types/schemaTypes.ts
@@ -3,3 +3,5 @@ import type { components } from '@schema';
 export type RecruitListResponse = components['schemas']['RecruitListResponse'];
 export type RecruitSimpleResponse = components['schemas']['RecruitSimpleResponse'];
 export type ResponseRecruitListResponse = components['schemas']['ResponseRecruitListResponse'];
+export type HelperProfileCreateRequest = components['schemas']['HelperProfileCreateRequest'];
+export type ImageCreateRequest = components['schemas']['ImageCreateRequest'];
