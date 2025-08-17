5b9cc79cdd2fcd239cd626f3ef362046ad19fa6d
yjbae3974 <brianbae6@gmail.com>
2025-08-18 00:29:02 +0900

    Feat: 타입 추가 및 반영

--- frontend/src/features/helper/types/schemaTypes.ts
+++ frontend/src/features/helper/types/schemaTypes.ts
@@ -1,8 +1,11 @@
 import type { components } from '@schema';
 
 export type RecruitListResponse = components['schemas']['RecruitListResponse'];
+export type RecruitDetailResponse = components['schemas']['RecruitDetailResponse'];
 export type RecruitSimpleResponse = components['schemas']['RecruitSimpleResponse'];
 export type ResponseRecruitListResponse = components['schemas']['ResponseRecruitListResponse'];
 export type HelperProfileCreateRequest = components['schemas']['HelperProfileCreateRequest'];
 export type ImageCreateRequest = components['schemas']['ImageCreateRequest'];
 export type CertificateInfo = components['schemas']['CertificateInfo'];
+export type RecruitStatus = components['schemas']['RecruitSimpleResponse']['recruitStatus'];
+export type EscortStatus = components['schemas']['RecruitSimpleResponse']['escortStatus'];
--- frontend/src/shared/types/schemaTypes.ts
+++ frontend/src/shared/types/schemaTypes.ts
@@ -1,3 +1,5 @@
 import type { components } from '@schema';
 
 export type ResponseVoid = components['schemas']['ResponseVoid'];
+export type RecruitStatus = components['schemas']['RecruitSimpleResponse']['recruitStatus'];
+export type EscortStatus = components['schemas']['RecruitSimpleResponse']['escortStatus'];
