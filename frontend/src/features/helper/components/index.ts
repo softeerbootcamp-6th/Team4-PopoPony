070db1a422dfcb7cf9d3d10fadb4c96a7e4c275f
TaeSeung Yoo <remicon99@gmail.com>
2025-08-18 23:51:57 +0900

    Feat: 컴포넌트 export

--- frontend/src/features/helper/components/index.ts
+++ frontend/src/features/helper/components/index.ts
@@ -1,6 +1,7 @@
 import Region from './profileSteps/Region';
 import Detail from './profileSteps/Detail';
 import RegionBottomSheet from './modal/RegionBottomSheet';
+import RegionBottomSheetForm from './modal/RegionBottomSheetForm';
 import CertificateImageUploader from './form/CertificateImageUploader';
 
-export { Region, Detail, RegionBottomSheet, CertificateImageUploader };
+export { Region, Detail, RegionBottomSheet, RegionBottomSheetForm, CertificateImageUploader };
