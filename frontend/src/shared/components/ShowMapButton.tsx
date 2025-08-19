608c902328cb45a420c14eba85df781e4277cd9b
TaeSeung Yoo <remicon99@gmail.com>
2025-08-19 14:48:49 +0900

    Feat: ShowMapButton 지도연결 추가

--- frontend/src/shared/components/ShowMapButton.tsx
+++ frontend/src/shared/components/ShowMapButton.tsx
@@ -4,9 +4,15 @@ interface ShowMapButtonProps {
   roadAddress?: string;
   businessAddress: string;
   pos: { lat: number; lng: number };
+  fontSize?: 'small' | 'medium';
 }
 
-const ShowMapButton = ({ roadAddress, businessAddress, pos }: ShowMapButtonProps) => {
+const ShowMapButton = ({
+  roadAddress,
+  businessAddress,
+  pos,
+  fontSize = 'small',
+}: ShowMapButtonProps) => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate({
@@ -20,8 +26,9 @@ const ShowMapButton = ({ roadAddress, businessAddress, pos }: ShowMapButtonProps
 
   return (
     <>
-      <div className='flex-wrap gap-[0.4rem]'>
-        <span className='body2-14-medium text-text-neutral-secondary mr-[0.4rem]'>{`${roadAddress ? `${roadAddress} ,` : ''} ${businessAddress}`}</span>
+      <div className='flex-wrap gap-[0.8rem]'>
+        <span
+          className={`${fontSize === 'small' ? 'body2-14-medium' : 'body1-16-medium'} text-text-neutral-secondary mr-[0.4rem]`}>{`${roadAddress ? `${roadAddress} ,` : ''} ${businessAddress}`}</span>
         <button
           className='caption2-10-medium text-text-neutral-secondary border-stroke-neutral-dark w-fit translate-y-[-0.1rem] rounded-[0.4rem] border px-[0.5rem] py-[0.2rem]'
           onClick={handleClick}>
