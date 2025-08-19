c71a2e6cfaf9c213f81b26ed624743541c9f06ba
yjbae3974 <brianbae6@gmail.com>
2025-08-19 14:29:57 +0900

    FIX: import component

--- frontend/src/features/dashboard/components/index.ts
+++ frontend/src/features/dashboard/components/index.ts
@@ -2,6 +2,7 @@ import Header from './Header';
 import DashBoardCard from './DashBoardCard';
 import EscortCompleted from './dashboardIndex/EscortCompleted';
 import WritingReport from './dashboardIndex/WritingReport';
-import DashboardLive from './dashboardIndex/DashboardLive';
+import CustomerDashboardLive from './dashboardIndex/CustomerDashboardLive';
+import Footer from './Footer';
 
-export { Header, DashBoardCard, EscortCompleted, WritingReport, DashboardLive };
+export { Header, DashBoardCard, EscortCompleted, WritingReport, CustomerDashboardLive, Footer };
