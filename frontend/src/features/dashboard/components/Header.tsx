4f6c3886643e5fea2a9d690647f3f7caea63b1b3
yjbae3974 <brianbae6@gmail.com>
2025-08-19 14:19:54 +0900

    Feat: Header 컴포넌트화

--- frontend/src/features/dashboard/components/Header.tsx
+++ frontend/src/features/dashboard/components/Header.tsx
@@ -2,21 +2,27 @@ import { useRouter } from '@tanstack/react-router';
 import { IcArrowLeft } from '@icons';
 
 type HeaderProps = {
+  showBack?: boolean;
+  showUpdateBefore?: boolean;
   updateBefore: number;
 };
 
-const Header = ({ updateBefore }: HeaderProps) => {
+const Header = ({ showBack = true, showUpdateBefore = true, updateBefore }: HeaderProps) => {
   const router = useRouter();
   return (
     <>
-      <button
-        className='bg-background-default-white shadow-button absolute top-[1.2rem] left-[1.2rem] flex h-[4.8rem] w-[4.8rem] items-center justify-center rounded-full'
-        onClick={() => router.history.back()}>
-        <IcArrowLeft className='h-[2.4rem] w-[2.4rem]' />
-      </button>
-      <div className='flex-center caption1-12-medium absolute top-[2.6rem] right-[1.2rem] rounded-[0.2rem] bg-[#1C1E220F] px-[0.4rem] py-[0.2rem] text-[#1C1E2299] backdrop-blur-[0.6rem]'>
-        {updateBefore}분 전 업데이트
-      </div>
+      {showBack && (
+        <button
+          className='bg-background-default-white shadow-button absolute top-[1.2rem] left-[1.2rem] flex h-[4.8rem] w-[4.8rem] items-center justify-center rounded-full'
+          onClick={() => router.history.back()}>
+          <IcArrowLeft className='h-[2.4rem] w-[2.4rem]' />
+        </button>
+      )}
+      {showUpdateBefore && (
+        <div className='flex-center caption1-12-medium absolute top-[2.6rem] right-[1.2rem] rounded-[0.2rem] bg-[#1C1E220F] px-[0.4rem] py-[0.2rem] text-[#1C1E2299] backdrop-blur-[0.6rem]'>
+          {updateBefore}분 전 업데이트
+        </div>
+      )}
     </>
   );
 };
