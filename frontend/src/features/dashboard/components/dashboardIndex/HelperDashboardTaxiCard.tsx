0c9713e0db838ac807a8ffebdcaea60357a4bf4e
yjbae3974 <brianbae6@gmail.com>
2025-08-20 11:11:24 +0900

    Feat: 택시 호출, 고객 요청 요약 카드 퍼블리시

---
+++ frontend/src/features/dashboard/components/dashboardIndex/HelperDashboardTaxiCard.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { DashBoardCard } from '@dashboard/components';
+import { IcTaxiCalled, IcTaxiWaiting } from '@icons';
+import { Button } from '@components';
+import { WarningBox } from '@customer/components';
+
+const HelperDashboardTaxiCard = ({
+  estimatedTaxiTime,
+  estimatedTaxiFee,
+}: {
+  estimatedTaxiTime: number;
+  estimatedTaxiFee: number;
+}) => {
+  const [texiStatus, setTaxiStatus] = useState<'called' | 'waiting'>('called');
+  return (
+    <DashBoardCard.Card>
+      <h4 className='title-20-bold text-text-neutral-primary'>택시를 호출하세요</h4>
+      <div className='flex w-full justify-center'>
+        {texiStatus === 'called' && <IcTaxiCalled className='w-full' />}
+        {texiStatus === 'waiting' && <IcTaxiWaiting className='w-full' />}
+      </div>
+      <div className='bg-neutral-20 mt-[2.8rem] flex w-full gap-[0.1rem]'>
+        <div className='bg-background-default-white flex-center flex flex-1 flex-col gap-[0.4rem]'>
+          <h6 className='body2-14-medium text-text-neutral-secondar'>예상 소요시간</h6>
+          <h6 className='subtitle-18-bold text-text-neutral-primary'>{estimatedTaxiTime}분</h6>
+        </div>
+        <div className='bg-background-default-white flex-center flex flex-1 flex-col gap-[0.4rem]'>
+          <h6 className='body2-14-medium text-text-neutral-secondary'>예상 금액</h6>
+          <h6 className='subtitle-18-bold text-text-neutral-primary'>
+            {estimatedTaxiFee.toLocaleString()}원
+          </h6>
+        </div>
+      </div>
+      <div className='mt-[1.6rem] w-full'>
+        {texiStatus === 'called' && (
+          <Button variant='secondary' size='md' onClick={() => setTaxiStatus('waiting')}>
+            택시 호출하기
+          </Button>
+        )}
+        {texiStatus === 'waiting' && (
+          <Button variant='assistive' size='md' onClick={() => setTaxiStatus('called')}>
+            취소하기
+          </Button>
+        )}
+      </div>
+      <WarningBox text='택시는 도우미님이 직접 결제하시고, 반드시 영수증을 받아주세요!' />
+    </DashBoardCard.Card>
+  );
+};
+
+export default HelperDashboardTaxiCard;
