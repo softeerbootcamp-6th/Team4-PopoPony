d51c6c03423efbcda0367a7519e55a95aea9a8a6
jaehyeonchoi <413cjh22@gmail.com>
2025-08-21 11:41:57 +0900

    Feat: LocationRequest - 생성자 및 빌더 패턴 추가

--- backend/server/src/main/java/com/todoc/server/domain/realtime/web/dto/request/LocationRequest.java
+++ backend/server/src/main/java/com/todoc/server/domain/realtime/web/dto/request/LocationRequest.java
@@ -1,5 +1,6 @@
 package com.todoc.server.domain.realtime.web.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,4 +12,10 @@ public class LocationRequest {
 
     private Double latitude;
     private Double longitude;
+
+    @Builder
+    public LocationRequest(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
