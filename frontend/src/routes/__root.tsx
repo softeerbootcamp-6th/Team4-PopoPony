95b32a4163f73e413ec4ea03617572e973ae8d38
yjbae3974 <brianbae6@gmail.com>
2025-08-23 20:44:27 +0900

    FIX: 에러바운더리, 홈으로 이동 눌러도 상태 안바뀌는 오류

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Outlet, createRootRouteWithContext, redirect } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { QueryClient } from '@tanstack/react-query';
-import { Landing, RootFallbackUI } from '@components';
+import { QueryErrorResetBoundary, type QueryClient } from '@tanstack/react-query';
+import { Landing, RootFallbackUI, SuspenseUI } from '@components';
 import { RootLayout } from '@layouts';
 import { authStorage } from '@auth/utils';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Suspense } from 'react';
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -33,9 +34,26 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <>
       <RootLayout>
         <Landing />
-        <ErrorBoundary fallbackRender={({ error }) => <RootFallbackUI error={error} />}>
-          <Outlet />
-        </ErrorBoundary>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              resetKeys={[location.pathname]}
+              fallbackRender={({ error, resetErrorBoundary }) => (
+                <RootFallbackUI
+                  error={error}
+                  onReset={() => {
+                    reset();
+                    resetErrorBoundary();
+                  }}
+                />
+              )}>
+              <Suspense fallback={<SuspenseUI />}>
+                <Outlet />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </RootLayout>
       <div className='text-[16px]'>
         <TanStackRouterDevtools />
