58d8cc10b274b883b502004f226695d5b03fd1b3
jaehyeonchoi <413cjh22@gmail.com>
2025-08-23 15:34:22 +0900

    Feat: 실시간 동행 상태 관련 요청/응답 DTO 정의

--- backend/server/src/main/java/com/todoc/server/domain/realtime/web/dto/response/LocationResponse.java
+++ backend/server/src/main/java/com/todoc/server/domain/realtime/web/dto/response/LocationResponse.java
@@ -1,19 +1,19 @@
 package com.todoc.server.domain.realtime.web.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Schema(description = "실시간 위치 응답 DTO")
 public class LocationResponse {
-
-    @Schema(description = "동행 ID")
-    private Long escortId;
 
     @Schema(description = "위도")
     private double latitude;
@@ -25,10 +25,17 @@ public class LocationResponse {
     private Instant timestamp;
 
     @Builder
-    public LocationResponse(Long escortId, double latitude, double longitude, Instant timestamp) {
-        this.escortId = escortId;
+    public LocationResponse(double latitude, double longitude, Instant timestamp) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.timestamp = timestamp;
+    }
+
+    public static LocationResponse from(LocationRedisDto dto) {
+        return LocationResponse.builder()
+                .latitude(dto.getLatitude())
+                .longitude(dto.getLongitude())
+                .timestamp(Instant.ofEpochMilli(dto.getTimestamp())) // millis → Instant
+                .build();
     }
 }
