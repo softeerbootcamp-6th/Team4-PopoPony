b2cb9bd5c49b46e1e22e9d59df7437146b031aad
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-08-23 16:29:37 +0900

    [HOTFIX] Fix: PresenceController - 새로운 구독자에 대한 알림을 GET 방식으로 받도록 설정 (#339)
    * FIX: 대시보드 레이아웃 및 택시 예상 시간
    
    * Chore: allowsOrigin 추가
    
    * Fix: 파라미터가 아예 없을 경우 오늘 이후의 동행을 조회하도록 수정
    
    * Test: 코드 리팩토링에 따른 테스트코드 수정
    
    * FIX: 환자용 대시보드 라우팅 수정
    
    * FIX: 리포트만 있고 리뷰 없어도 도우미 화면 들어갈 수 있게끔
    
    * FIX:리뷰 없어도 리포트 뜨도록 수정(helper)
    
    * FIX: 보호자 사이드 시간 없으면 alert 안 뜨도록
    
    * FIX: 환자 대시보드는 로그인 안해도 되도록
    
    * Refactor: LocationRequest - 불필요한 클래스 삭제
    
    * Feat: 실시간 동행 상태 관련 요청/응답 DTO 정의
    
    * Feat: WebSocketMsgType - 웹소켓 메세지 타입 Enum 정의
    
    * Feat: WebSocket 통신 관련 Controller, Service, Repository 계층 클래스 구현
    
    * Feat: SSE 방식도 정상 작동하도록 파라미터 형식 일치
    
    * Feat: Redis Lua로 실시간 위치 정보 검증 및 필터링 기능 구현
    
    * Feat: Nchan을 통한 브로드캐스팅 기능 구현
    
    * Refactor: 불필요한 코드 제거
    
    * Fix: EscortServiceTest - 기존 테스트 코드와의 충돌 해결
    
    * Fix: PresenceController - 새로운 구독자에 대한 알림을 GET 방식으로 받도록 설정
    
    ---------
    
    Co-authored-by: yjbae3974 <brianbae6@gmail.com>
    Co-authored-by: seonghooni <lsh3717@naver.com>
    Co-authored-by: 배연준 <49051494+yjbae3974@users.noreply.github.com>

--- backend/server/src/main/java/com/todoc/server/domain/realtime/web/controller/PresenceController.java
+++ backend/server/src/main/java/com/todoc/server/domain/realtime/web/controller/PresenceController.java
@@ -5,10 +5,7 @@ import com.todoc.server.domain.realtime.service.NchanPublisher;
 import com.todoc.server.domain.realtime.service.WebSocketFacadeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
--- frontend/src/features/helper/components/tabs/ReportTab.tsx
+++ frontend/src/features/helper/components/tabs/ReportTab.tsx
@@ -1,4 +1,4 @@
-import { Tabs, Divider, Spinner } from '@components';
+import { Tabs, Divider } from '@components';
 import { InfoSection, WarningBox } from '@customer/components';
 import { IcAlertCircle } from '@icons';
 import { getRecruitReportByRecruitId, getRecruitReviewByRecruitId } from '@helper/apis';
@@ -15,112 +15,104 @@ const ReportTab = () => {
   const { escortId } = useParams({ from: '/helper/escort/$escortId/' });
   const { data: reportData } = getRecruitReportByRecruitId(Number(escortId));
   const isValidReport = reportData?.status === 200;
-  if (reportData && !isValidReport) {
-    alert(reportData.message || '리포트 조회에 실패했습니다.');
-    return null;
-  }
 
-  const { data: reviewData, isLoading: isReviewDataLoading } = getRecruitReviewByRecruitId(
-    Number(escortId),
-    Boolean(isValidReport)
-  );
+  const { data: reviewData } = getRecruitReviewByRecruitId(Number(escortId), true);
 
   const actualStartTime = reportData?.data?.actualMeetingTime;
   const actualEndTime = reportData?.data?.actualReturnTime;
-
-  if (isReviewDataLoading) return <Spinner />;
 
   return (
     <>
       <Tabs.TabsContentSection>
         {reviewData && reviewData.status === 200 && (
-          <>
-            <div>
-              {reviewData.status === 200 && (
-                <div className='border-stroke-neutral-dark flex flex-col gap-[1.2rem] rounded-[0.8rem] border px-[1.6rem] py-[1.2rem]'>
-                  <div className='flex-start gap-[1.2rem]'>
-                    <img
-                      src={`/images/status-${statusMap[reviewData?.data?.satisfactionLevel as keyof typeof statusMap]}.svg`}
-                      alt={`status-${statusMap[reviewData?.data?.satisfactionLevel as keyof typeof statusMap]}`}
-                      className='h-[4rem] w-[4rem]'
-                    />
-                    <div className='flex flex-col gap-[0.2rem]'>
-                      <h6 className='body2-14-bold text-text-neutral-primary'>
-                        {reviewData?.data?.satisfactionLevel}
-                      </h6>
-                      <p className='body2-14-medium text-text-neutral-primary'>
-                        {reviewData?.data?.shortComment}
-                      </p>
-                    </div>
-                  </div>
-                  <div className='bg-stroke-neutral-light h-[0.1rem] w-full rounded-full'></div>
-                  <div className='flex-start gap-[2.8rem]'>
-                    {reviewData.data?.positiveFeedbackList &&
-                      reviewData.data?.positiveFeedbackList?.length > 0 && (
-                        <span className='label2-14-bold text-text-neutral-secondary'>좋은 점</span>
-                      )}
-                    {reviewData.data?.positiveFeedbackList &&
-                      reviewData.data?.positiveFeedbackList?.length > 0 &&
-                      reviewData.data?.positiveFeedbackList?.map((item, index) => (
-                        <>
-                          <div className='flex-start flex-wrap gap-[0.4rem]'>
-                            <div
-                              className='flex-center label2-14-medium bg-neutral-10 text-text-neutral-secondary h-[2.8rem] rounded-[0.4rem] px-[0.8rem]'
-                              key={index}>
-                              {item}
-                            </div>
-                          </div>
-                        </>
-                      ))}
-                  </div>
+          <div>
+            <div className='border-stroke-neutral-dark flex flex-col gap-[1.2rem] rounded-[0.8rem] border px-[1.6rem] py-[1.2rem]'>
+              <div className='flex-start gap-[1.2rem]'>
+                <img
+                  src={`/images/status-${statusMap[reviewData?.data?.satisfactionLevel as keyof typeof statusMap]}.svg`}
+                  alt={`status-${statusMap[reviewData?.data?.satisfactionLevel as keyof typeof statusMap]}`}
+                  className='h-[4rem] w-[4rem]'
+                />
+                <div className='flex flex-col gap-[0.2rem]'>
+                  <h6 className='body2-14-bold text-text-neutral-primary'>
+                    {reviewData?.data?.satisfactionLevel}
+                  </h6>
+                  <p className='body2-14-medium text-text-neutral-primary'>
+                    {reviewData?.data?.shortComment}
+                  </p>
                 </div>
-              )}
-              <h3 className='subtitle-18-bold text-text-neutral-primary mt-[2rem]'>동행 리포트</h3>
-              <div className='mt-[1.2rem] flex flex-col gap-[2rem]'>
-                <InfoSection title='진행 시간'>
-                  {actualStartTime && actualEndTime ? (
-                    <span className='body1-16-bold text-text-neutral-primary'>
-                      {timeFormat(actualStartTime, 'aaa HH:mm')} ~{' '}
-                      {timeFormat(actualEndTime, 'aaa HH:mm')}
-                    </span>
-                  ) : (
-                    <span className='body1-16-medium text-text-neutral-secondary'>-</span>
-                  )}
-                  {reportData?.data?.extraMinutes && reportData?.data?.extraMinutes > 0 ? (
-                    <div className='flex-start gap-[0.4rem]'>
-                      <IcAlertCircle
-                        className='[&_path]:fill-icon-red-primary'
-                        width={16}
-                        height={16}
-                      />
-                      <span className='label3-12-medium text-text-red-primary'>
-                        예상 동행 시간보다 {reportData?.data?.extraMinutes}분 초과되었어요!
-                      </span>
-                    </div>
-                  ) : null}
-                </InfoSection>
-                <Divider />
-                {reportData?.data?.hasNextAppointment && (
-                  <>
-                    <InfoSection title='다음 예약'>
-                      <span className='body1-16-medium text-text-neutral-primary'>
-                        {reportData?.data?.nextAppointmentTime &&
-                          dateFormat(
-                            reportData?.data?.nextAppointmentTime,
-                            'yyyy년 MM월 dd일 aaa HH시 mm분'
-                          )}
-                      </span>
-                    </InfoSection>
-                    <Divider />
-                  </>
-                )}
-
-                <InfoSection title='전달 내용'>
-                  <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col rounded-[0.6rem] p-[1rem]'>
-                    <span>{reportData?.data?.description}</span>
-                  </div>
-                </InfoSection>
               </div>
+              <div className='bg-stroke-neutral-light h-[0.1rem] w-full rounded-full'></div>
+              <div className='flex-start gap-[2.8rem]'>
+                {reviewData.data?.positiveFeedbackList &&
+                  reviewData.data?.positiveFeedbackList?.length > 0 && (
+                    <span className='label2-14-bold text-text-neutral-secondary'>좋은 점</span>
+                  )}
+                {reviewData.data?.positiveFeedbackList &&
+                  reviewData.data?.positiveFeedbackList?.length > 0 &&
+                  reviewData.data?.positiveFeedbackList?.map((item, index) => (
+                    <>
+                      <div className='flex-start flex-wrap gap-[0.4rem]'>
+                        <div
+                          className='flex-center label2-14-medium bg-neutral-10 text-text-neutral-secondary h-[2.8rem] rounded-[0.4rem] px-[0.8rem]'
+                          key={index}>
+                          {item}
+                        </div>
+                      </div>
+                    </>
+                  ))}
+              </div>
+            </div>
+          </div>
+        )}
+
+        {isValidReport && (
+          <>
+            <h3 className='subtitle-18-bold text-text-neutral-primary mt-[2rem]'>동행 리포트</h3>
+            <div className='mt-[1.2rem] flex flex-col gap-[2rem]'>
+              <InfoSection title='진행 시간'>
+                {actualStartTime && actualEndTime ? (
+                  <span className='body1-16-bold text-text-neutral-primary'>
+                    {timeFormat(actualStartTime, 'aaa HH:mm')} ~{' '}
+                    {timeFormat(actualEndTime, 'aaa HH:mm')}
+                  </span>
+                ) : (
+                  <span className='body1-16-medium text-text-neutral-secondary'>-</span>
+                )}
+                {reportData?.data?.extraMinutes && reportData?.data?.extraMinutes > 0 ? (
+                  <div className='flex-start gap-[0.4rem]'>
+                    <IcAlertCircle
+                      className='[&_path]:fill-icon-red-primary'
+                      width={16}
+                      height={16}
+                    />
+                    <span className='label3-12-medium text-text-red-primary'>
+                      예상 동행 시간보다 {reportData?.data?.extraMinutes}분 초과되었어요!
+                    </span>
+                  </div>
+                ) : null}
+              </InfoSection>
+              <Divider />
+              {reportData?.data?.hasNextAppointment && (
+                <>
+                  <InfoSection title='다음 예약'>
+                    <span className='body1-16-medium text-text-neutral-primary'>
+                      {reportData?.data?.nextAppointmentTime &&
+                        dateFormat(
+                          reportData?.data?.nextAppointmentTime,
+                          'yyyy년 MM월 dd일 aaa HH시 mm분'
+                        )}
+                    </span>
+                  </InfoSection>
+                  <Divider />
+                </>
+              )}
+
+              <InfoSection title='전달 내용'>
+                <div className='bg-neutral-10 label2-14-medium text-text-neutral-primary flex flex-col rounded-[0.6rem] p-[1rem]'>
+                  <span>{reportData?.data?.description || '-'}</span>
+                </div>
+              </InfoSection>
             </div>
 
             {/* 이용 금액 */}
