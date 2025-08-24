7c752fd30c34b032894c7ad249558c4ead82840f
yjbae3974 <brianbae6@gmail.com>
2025-08-24 18:10:33 +0900

    Feat: 에러 바운더리 UI 수정

--- frontend/src/shared/components/catchThrow/FallbackUI.tsx
+++ frontend/src/shared/components/catchThrow/FallbackUI.tsx
@@ -1,17 +1,50 @@
 import { IcAlertCircle } from '@icons';
 import { Button } from '@components';
+import { ApiError, AuthError, NotFoundError } from '@apis';
+import { useNavigate } from '@tanstack/react-router';
 
 const FallbackUI = ({ error, onReset }: { error: Error; onReset?: () => void }) => {
-  return (
-    <div className='flex h-full w-full flex-col items-center gap-2 p-[5rem]'>
-      <IcAlertCircle className='text-text-red-primary' />
-      <p className='body1-16-medium text-text-neutral-primary'>에러가 발생했습니다.</p>
-      <p className='body1-16-medium text-text-neutral-primary'>{error.message}</p>
-      <div className='bg-bg-neutral-secondary w-full p-[2rem]'>
-        <Button size='md' onClick={onReset}>
-          다시 시도
+  const navigate = useNavigate();
+
+  const handleClickHome = () => {
+    if (onReset) onReset();
+    navigate({ to: '/' });
+  };
+  const handleClickLogin = () => {
+    if (onReset) onReset();
+    navigate({ to: '/login' });
+  };
+  const handleClickRefresh = () => {
+    if (onReset) onReset();
+    window.location.reload();
+  };
+
+  const ButtonToSolution = () => {
+    if (error instanceof AuthError)
+      return (
+        <Button size='md' onClick={handleClickLogin}>
+          로그인 화면으로 가기
         </Button>
-      </div>
+      );
+    if (error instanceof ApiError || error instanceof NotFoundError)
+      return (
+        <Button size='md' onClick={handleClickHome}>
+          홈으로 이동
+        </Button>
+      );
+    return (
+      <Button size='md' onClick={handleClickRefresh}>
+        새로고침
+      </Button>
+    );
+  };
+
+  return (
+    <div className='flex h-full w-full flex-col items-center gap-[2rem] p-[5rem] text-center'>
+      <img className='w-[50%]' src='/images/report.svg' alt='오류 이미지' />
+      <p className='title-20-bold text-text-neutral-primary'>일시적인 오류가 발생했습니다..</p>
+      <p className='body1-16-medium text-text-neutral-primary'>{error.message}</p>
+      <ButtonToSolution />
     </div>
   );
 };
--- frontend/src/shared/components/catchThrow/RootFallbackUI.tsx
+++ frontend/src/shared/components/catchThrow/RootFallbackUI.tsx
@@ -1,25 +1,51 @@
 import { useNavigate } from '@tanstack/react-router';
-import { IcAlertCircle } from '@icons';
 import { Button } from '@components';
+import { ApiError, AuthError, NotFoundError } from '@apis';
 
 const RootFallbackUI = ({ error, onReset }: { error: Error; onReset?: () => void }) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClickHome = () => {
     if (onReset) onReset();
     navigate({ to: '/' });
+  };
+  const handleClickLogin = () => {
+    if (onReset) onReset();
+    navigate({ to: '/login' });
+  };
+  const handleClickRefresh = () => {
+    if (onReset) onReset();
+    window.location.reload();
+  };
+
+  const ButtonToSolution = () => {
+    if (error instanceof AuthError)
+      return (
+        <Button size='md' onClick={handleClickLogin}>
+          로그인 화면으로 가기
+        </Button>
+      );
+    if (error instanceof ApiError || error instanceof NotFoundError)
+      return (
+        <Button size='md' onClick={handleClickHome}>
+          홈으로 이동
+        </Button>
+      );
+    return (
+      <Button size='md' onClick={handleClickRefresh}>
+        새로고침
+      </Button>
+    );
   };
 
   return (
     <>
       <div className='flex-center h-full w-full flex-col gap-[2rem] p-[5rem] text-center'>
-        <IcAlertCircle className='text-text-red-primary' />
-        <p className='body1-16-medium text-text-neutral-primary'>에러가 발생했습니다.</p>
-        <p className='body1-16-medium text-text-neutral-primary'>{error.name}</p>
+        <img className='w-[50%]' src='/images/report.svg' alt='오류 이미지' />
+        <p className='headline-24-bold text-text-neutral-primary'>일시적인 오류가 발생했습니다..</p>
+        {/* <p className='body1-16-medium text-text-neutral-secondary'>에러 종류: {error.name}</p> */}
         <p className='body1-16-medium text-text-neutral-primary'>{error.message}</p>
-        <Button size='md' onClick={handleClick}>
-          홈으로 이동
-        </Button>
+        <ButtonToSolution />
       </div>
     </>
   );
