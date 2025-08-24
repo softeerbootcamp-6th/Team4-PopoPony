ac6d347602b62b06a5860301bbea6ccc9074daf7
yjbae3974 <brianbae6@gmail.com>
2025-08-24 17:30:01 +0900

    Feat: api 일부 gcTime, staleTime 설정

--- frontend/src/features/customer/apis/getRecruitReportByRecruitId.ts
+++ frontend/src/features/customer/apis/getRecruitReportByRecruitId.ts
@@ -1,13 +1,18 @@
 import { $api } from '@apis';
 
 const getRecruitReviewByRecruitId = (recruitId: number) => {
-  return $api.useQuery('get', '/api/reports/recruits/{recruitId}', {
-    params: {
-      path: {
-        recruitId: Number(recruitId),
+  return $api.useQuery(
+    'get',
+    '/api/reports/recruits/{recruitId}',
+    {
+      params: {
+        path: {
+          recruitId: Number(recruitId),
+        },
       },
     },
-  });
+    { throwOnError: false, staleTime: 30_000, gcTime: 300_000 }
+  );
 };
 
 export default getRecruitReviewByRecruitId;
